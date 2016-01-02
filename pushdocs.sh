7cb75ee545e7d7f45aed2611e258c84929071f11
Eyal Kalderon <ebkalderon@gmail.com>
2016-01-02 13:15:09 +0200

    move doc gen code to pushdocs.sh

---
+++ pushdocs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp -r book/html/ web/
+cp -r target/doc/ web/doc/
+
+sudo pip install ghp-import
+ghp-import -n web/
+
+git config user.name "Eyal Kalderon"
+git config user.email "ebkalderon@gmail.com"
+git push -fq https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
