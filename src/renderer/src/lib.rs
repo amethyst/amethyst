58bf984edd799b823d09ade2b6cdd04c5c9e5992
Eyal Kalderon <ebkalderon@gmail.com>
2016-01-26 14:07:57 -0500

    add crate attributes and API exports

--- src/renderer/src/lib.rs
+++ src/renderer/src/lib.rs
@@ -1,7 +1,14 @@
+#![crate_name = "amethyst_renderer"]
+#![crate_type = "lib"]
+#![doc(html_logo_url = "http://tinyurl.com/hgsb45k")]
+
 //! High-level rendering engine with multiple backends.
 
 extern crate gfx;
 
-pub mod backend;
-pub mod frontend;
-pub mod ir;
+mod backend;
+mod frontend;
+mod ir;
+
+pub use self::backend::Backend;
+pub use self::frontend::Frontend;
