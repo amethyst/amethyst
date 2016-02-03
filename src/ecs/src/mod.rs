8c5cecf1c4789e31378070bffc1d98eb0cd7a73b
Eyal Kalderon <ebkalderon@gmail.com>
2016-02-03 14:23:55 -0500

    remove mod.rs

--- src/ecs/src/mod.rs
+++
@@ -1,9 +0,0 @@
-//! The entity-component-system framework.
-
-mod component;
-mod entity;
-mod world;
-
-pub use self::entity::Entity;
-pub use self::component::Component;
-pub use self::world::World;
