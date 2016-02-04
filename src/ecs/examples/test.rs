e38c33bfc5f5403fecd1ab38191f3b7ee096ce31
Oflor <oflorek@gmail.com>
2016-02-04 13:10:14 +0300

    Silly me, forgot to add the file. Added get_component method to World and its usage to the example

--- src/ecs/examples/test.rs
+++ src/ecs/examples/test.rs
@@ -1,6 +1,7 @@
 extern crate amethyst_ecs;
 use amethyst_ecs::*;
 
+#[derive(Debug)]
 struct Position {
     x: f32,
     y: f32,
@@ -21,10 +22,11 @@ struct Mesh {
 fn main() {
     let mut world = World::new();
     
-    for _ in 0..180 {
+    for i in 0..180 {
         let ent = world.create_entity();
-        world.insert_component(ent, Position { x: 0.0, y: 0.0, z: 0.0 });
+        world.insert_component(ent, Position { x: i as f32 * 0.1, y: 0.0, z: 0.0 });
         world.insert_component(ent, Light { x: 0.0, y: 0.0, z: 0.0 });
         world.insert_component(ent, Mesh { handle: 1234567890, y: 12 });
     }
+    println!("Component Position #{}: {:?}", 60, world.get_component::<Position>(60).unwrap());
 }
