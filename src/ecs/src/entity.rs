492537875e71f09045d9e7d25a8b304fbcc5f9af
Eyal Kalderon <ebkalderon@gmail.com>
2016-02-04 16:15:19 -0500

    fix entity ID recycling issue

--- src/ecs/src/entity.rs
+++ src/ecs/src/entity.rs
@@ -6,7 +6,6 @@ pub type Entity = u64;
 /// Manages the creation and destruction of entities.
 #[derive(Debug)]
 pub struct Entities {
-    alive: Vec<Entity>,
     dead: Vec<Entity>,
     next_id: Entity,
 }
@@ -15,7 +14,6 @@ impl Entities {
     /// Creates a new entity manager.
     pub fn new() -> Entities {
         Entities {
-            alive: Vec::new(),
             dead: Vec::new(),
             next_id: 0,
         }
@@ -24,12 +22,10 @@ impl Entities {
     /// Creates a new entity and returns its handle.
     pub fn create(&mut self) -> Entity {
         if let Some(id) = self.dead.pop() {
-            self.alive.push(id.clone());
             return id;
         }
 
         let new_entity = self.next_id;
-        self.alive.push(new_entity.clone());
         self.next_id += 1;
 
         new_entity
@@ -37,19 +33,61 @@ impl Entities {
 
     /// Checks whether the given entity is alive.
     pub fn is_alive(&self, entity: Entity) -> bool {
-        self.alive.contains(&entity)
+        self.next_id > entity && !self.dead.contains(&entity)
     }
 
     /// Checks how many entities are currently in the world.
     pub fn num_alive(&self) -> usize {
-        self.alive.len()
+        self.next_id as usize - self.dead.len()
     }
 
     /// Destroys the given entity.
     pub fn destroy(&mut self, entity: Entity) {
         if self.is_alive(entity) {
-            self.alive.retain(|&e| e != entity);
-            self.dead.push(entity);
+            self.dead.insert(0, entity);
         }
+    }
+}
+
+// Unit tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn creation() {
+        let mut ents = Entities::new();
+
+        let first = ents.create();
+        assert_eq!(first, 0);
+
+        let second = ents.create();
+        assert_eq!(second, 1);
+    }
+
+    #[test]
+    fn recycling() {
+        let mut ents = Entities::new();
+
+        let first = ents.create();
+        assert_eq!(first, 0);
+
+        let second = ents.create();
+        assert_eq!(second, 1);
+
+        let third = ents.create();
+        assert_eq!(third, 2);
+
+        ents.destroy(first);
+        assert!(!ents.is_alive(first));
+
+        ents.destroy(third);
+        assert!(!ents.is_alive(third));
+
+        let rec_first = ents.create();
+        assert_eq!(rec_first, 0);
+
+        let rec_second = ents.create();
+        assert_eq!(rec_second, 2);
     }
 }
