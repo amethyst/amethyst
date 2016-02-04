594e60bdb6f69a98aa252fc3af1f00ec22fe3765
Eyal Kalderon <ebkalderon@gmail.com>
2016-02-03 22:12:13 -0500

    add AnyVec, reimplement World, add example and test

---
+++ src/ecs/examples/test.rs
@@ -0,0 +1,30 @@
+extern crate amethyst_ecs;
+use amethyst_ecs::*;
+
+struct Position {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+struct Light {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+struct Mesh {
+    handle: u64,
+    y: usize,
+}
+
+fn main() {
+    let mut world = World::new();
+    
+    for _ in 0..180 {
+        let ent = world.create_entity();
+        world.insert_component(ent, Position { x: 0.0, y: 0.0, z: 0.0 });
+        world.insert_component(ent, Light { x: 0.0, y: 0.0, z: 0.0 });
+        world.insert_component(ent, Mesh { handle: 1234567890, y: 12 });
+    }
+}
--- src/ecs/src/world.rs
+++ src/ecs/src/world.rs
@@ -1,52 +1,49 @@
 //! The game world in which entities reside.
 
-use super::component::Component;
+use super::anyvec::AnyVec;
 use super::entity::{Entity, Entities};
 
 use std::any::Any;
+use std::collections::BTreeMap;
+
+type Components = BTreeMap<Entity, AnyVec>;
 
 /// A collection of entities and their respective components.
 #[derive(Debug)]
 pub struct World {
+    components: Components,
     entities: Entities,
-    components: Vec<Component>,
 }
 
 impl World {
     /// Creates a new empty world.
     pub fn new() -> World {
         World {
-            components: Vec::new(),
+            components: Components::new(),
             entities: Entities::new(),
         }
     }
 
     /// Creates a new entity in the world and returns a handle to it.
     pub fn create_entity(&mut self) -> Entity {
-        self.entities.create()
+        let id = self.entities.create();
+        self.components.insert(id.clone(), AnyVec::new());
+        id
     }
 
     /// Destroys a given entity and deallocates its components.
     pub fn destroy_entity(&mut self, entity: Entity) {
+        self.components.remove(&entity);
         self.entities.destroy(entity);
-        self.components.retain(|c| (*c).owner != entity);
     }
 
     /// Attaches a component to an entity.
     pub fn insert_component<T: Any>(&mut self, entity: Entity, comp: T) {
         if self.entities.is_alive(entity) {
-            self.components.push(Component::new(entity, comp));
-            self.components.sort_by(|next, prev| next.owner.cmp(&prev.owner));
+            if let Some(c) = self.components.get_mut(&entity) {
+                c.push(comp);
+            }
         }
     }
-
-    /// Gets an immutable reference to all of the components in the world.
-    pub fn get_components(&self) -> &Vec<Component> {
-        &self.components
-    }
-
-    /// Gets a mutable reference to all of the components in the world.
-    pub fn get_components_mut(&mut self) -> &mut Vec<Component> {
-        &mut self.components
-    }
 }
+
