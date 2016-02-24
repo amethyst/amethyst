64d912df14c8b47857fe627acb75b047c3750688
Eyal Kalderon <ebkalderon@gmail.com>
2016-02-24 12:03:34 -0500

    Let Backend accept GFX SubmitInfo struct as input

--- src/renderer/src/backend.rs
+++ src/renderer/src/backend.rs
@@ -1,5 +1,8 @@
 //! Consumes command buffers and executes them in the correct order.
 
+use gfx::Device;
+use gfx::SubmitInfo;
+
 /// Makes low-level graphics API calls and manages memory.
 pub struct Backend;
 
@@ -7,5 +10,9 @@ impl Backend {
     /// Creates a new renderer backend.
     pub fn new() -> Backend {
         Backend
+    }
+
+    pub fn submit<D: Device>(buf: &SubmitInfo<D>) {
+        unimplemented!()
     }
 }
