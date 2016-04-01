eb776c31146dfc212c01c920547a20387595dfde
Colin Sherratt <colin.sherratt@gmail.com>
2016-04-01 00:06:37 -0400

    first crack at the deffered render

---
+++ src/renderer/src/forward.rs
@@ -0,0 +1,96 @@
+
+use gfx;
+use gfx::traits::FactoryExt;
+
+pub static FORWARD_VERTEX_SRC: &'static [u8] = b"
+    #version 150 core
+
+    layout(std140)
+    uniform FowardVertexUniforms {
+        mat4 u_Model;
+        mat4 u_View;
+        mat4 u_Proj;
+    };
+
+    in vec3 a_Pos;
+    in vec3 a_Normal;
+    in vec3 a_Color;
+
+    out vec3 v_FragPos;
+    out vec3 v_Normal;
+
+    void main() {
+        v_FragPos = (u_Model * vec4(a_Pos, 1.0)).xyz;
+        v_Normal = mat3(u_Model) * a_Normal;
+        v_Color = a_Color;
+        gl_Position = u_Proj * u_View * u_Model * vec4(a_Pos, 1.0);
+    }
+";
+
+pub static FORWARD_FLAT_FRAGMENT_SRC: &'static [u8] = b"
+    #version 150 core
+
+    uniform FlatFragmentUniforms {
+        vec4 u_Ka;
+        vec4 u_Kd;
+    };
+
+    in vec3 v_FragPos;
+    in vec3 v_Normal;
+
+    out vec4 o_Normal;
+    out vec4 o_Ka;
+    out vec4 o_Kd;
+
+    void main() {
+        vec3 n = normalize(v_Normal);
+
+        o_Normal = vec4(n, 0.0);
+        o_Color = vec4(v_Color, 1.0);
+    }
+";
+
+pub type GFormat = [f32; 4];
+
+// placeholder
+gfx_vertex_struct!( VertexPosNormal {
+    pos: [f32; 3] = "a_Pos",
+    normal: [f32; 3] = "a_Normal",
+});
+
+gfx_constant_struct!( VertexUniforms {
+    model: [[f32; 4]; 4] = "u_Model",
+    view: [[f32; 4]; 4] = "u_View",
+    proj: [[f32; 4]; 4] = "u_Proj",
+});
+
+gfx_constant_struct!( FlatFragmentUniforms {
+    ka: [f32; 4] = "u_Ka",
+    kd: [f32; 4] = "u_Kd",
+});
+
+gfx_pipeline!( flat {
+    vbuf: gfx::VertexBuffer<VertexPosNormal> = (),
+    uniform_vs: gfx::ConstantBuffer<VertexUniforms> = "FowardVertexUniforms",
+    uniform_fs: gfx::ConstantBuffer<FlatFragmentUniforms> = "FlatFragmentUniforms",
+
+    out_normal: gfx::RenderTarget<GFormat> = "Target0",
+    out_ka: gfx::RenderTarget<GFormat> = "Target1",
+    out_kd: gfx::RenderTarget<GFormat> = "Target2",
+    out_depth: gfx::DepthTarget<gfx::format::Depth> =
+        gfx::preset::depth::LESS_EQUAL_WRITE,
+});
+
+pub type FlatPipeline<R> = gfx::pso::PipelineState<R, flat::Meta>;
+
+pub fn create_flat_pipeline<F, R>(factory: &mut F) -> FlatPipeline<R>
+    where R: gfx::Resources,
+          F: gfx::Factory<R>
+{
+    factory.create_pipeline_simple(
+        FORWARD_VERTEX_SRC,
+        FORWARD_FLAT_FRAGMENT_SRC,
+        gfx::state::CullFace::Back,
+        flat::new()
+    ).unwrap()
+}
--- src/renderer/src/lib.rs
+++ src/renderer/src/lib.rs
@@ -4,13 +4,134 @@
 
 //! High-level rendering engine with multiple backends.
 
+#[macro_use]
 extern crate gfx;
+extern crate glutin;
 
-mod backend;
-mod frame;
-mod frontend;
-mod pipeline;
+mod forward;
+use gfx::traits::FactoryExt;
 
-pub use self::frame::{Frame, FrameBuilder};
-pub use self::frontend::Renderer;
-pub use self::pipeline::{Pipeline, PipelineBuilder, Stage, Step};
+pub struct GfxRenderer<R: gfx::Resources> {
+    pipeline_foward: forward::FlatPipeline<R>,
+    flat_uniform_vs: gfx::handle::Buffer<R, forward::VertexUniforms>,
+    flat_uniform_fs: gfx::handle::Buffer<R, forward::FlatFragmentUniforms>,
+
+    gbuf_target: GBufferTarget<R>,
+}
+
+struct GBufferTarget<R: gfx::Resources> {
+    normal: gfx::handle::RenderTargetView<R, [f32; 4]>,
+    ka: gfx::handle::RenderTargetView<R, [f32; 4]>,
+    kd: gfx::handle::RenderTargetView<R, [f32; 4]>,
+    depth: gfx::handle::DepthStencilView<R, gfx::format::Depth>,
+}
+
+impl<R> GBufferTarget<R>
+    where R: gfx::Resources
+{
+    fn new<F>(factory: &mut F, (width, height): (u16, u16)) -> Self
+        where F: gfx::Factory<R>
+    {
+        let (_ , _, normal) = factory.create_render_target(width, height).unwrap();
+        let (_ , _, ka) = factory.create_render_target(width, height).unwrap();
+        let (_ , _, kd) = factory.create_render_target(width, height).unwrap();
+        let (_, _, depth) = factory.create_depth_stencil(width, height).unwrap();
+
+        GBufferTarget{
+            normal: normal,
+            ka: ka,
+            kd: kd,
+            depth: depth
+        }
+    }
+}
+
+impl<R> GfxRenderer<R>
+    where R: gfx::Resources
+{
+    pub fn new<F>(mut factory: F) -> GfxRenderer<R>
+        where F: gfx::Factory<R>
+    {
+        let pipeline_foward = forward::create_flat_pipeline(&mut factory);
+        let gbuf_target = GBufferTarget::new(&mut factory, (800, 600));
+        let flat_uniform_vs = factory.create_constant_buffer(1);
+        let flat_uniform_fs = factory.create_constant_buffer(1);
+        GfxRenderer {
+            pipeline_foward: pipeline_foward,
+            gbuf_target: gbuf_target,
+            flat_uniform_vs: flat_uniform_vs,
+            flat_uniform_fs: flat_uniform_fs,
+        }
+    }
+
+    pub fn render<C>(&mut self, scene: &Scene<R, forward::VertexPosNormal>, encoder: &mut gfx::Encoder<R, C>)
+        where C: gfx::CommandBuffer<R>
+    {
+
+        // clear the gbuffer
+        encoder.clear(&self.gbuf_target.normal, [0.; 4]);
+        encoder.clear(&self.gbuf_target.ka, [0.; 4]);
+        encoder.clear(&self.gbuf_target.kd, [0.; 4]);
+        encoder.clear_depth(&self.gbuf_target.depth, 1.0);
+
+        // every entity gets drawn
+        for e in &scene.entities {
+            encoder.update_constant_buffer(&self.flat_uniform_vs,
+                &forward::VertexUniforms {
+                    view: scene.view,
+                    proj: scene.projection,
+                    model: e.transform
+                }
+            );
+            encoder.update_constant_buffer(&self.flat_uniform_fs,
+                &forward::FlatFragmentUniforms{
+                    ka: e.ka,
+                    kd: e.kd
+                }
+            );
+
+            let data = forward::flat::Data {
+                vbuf: e.buffer.clone(),
+                uniform_vs: self.flat_uniform_vs.clone(),
+                uniform_fs: self.flat_uniform_fs.clone(),
+                out_normal: self.gbuf_target.normal.clone(),
+                out_ka: self.gbuf_target.ka.clone(),
+                out_kd: self.gbuf_target.kd.clone(),
+                out_depth: self.gbuf_target.depth.clone()
+            };
+
+            encoder.draw(
+                &e.slice,
+                &self.pipeline_foward,
+                &forward::flat::Data {
+                    vbuf: e.buffer.clone(),
+                    uniform_vs: self.flat_uniform_vs.clone(),
+                    uniform_fs: self.flat_uniform_fs.clone(),
+                    out_normal: self.gbuf_target.normal.clone(),
+                    out_ka: self.gbuf_target.ka.clone(),
+                    out_kd: self.gbuf_target.kd.clone(),
+                    out_depth: self.gbuf_target.depth.clone()
+                }
+            );
+        }
+    }
+}
+
+// placeholder Entity
+pub struct Entity<R: gfx::Resources, T> {
+    pub transform: [[f32; 4]; 4],
+    pub buffer: gfx::handle::Buffer<R, T>,
+    pub slice: gfx::Slice<R>,
+    // ambient colour
+    pub ka: [f32; 4],
+    // diffuse colour
+    pub kd: [f32; 4]
+}
+
+// this is a placeholder until we get a working ECS
+pub struct Scene<R: gfx::Resources, T> {
+    pub projection: [[f32; 4]; 4],
+    pub view: [[f32; 4]; 4],
+    pub entities: Vec<Entity<R, T>>,
+}
+>>>>>>> first crack at the deffered render
