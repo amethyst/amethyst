f3daa4739eb333a3436e5079b5cda0fdf1637f3a
= <palodequeso@gmail.com>
2016-05-12 18:36:47 -0400

    Fix forward renderer on more strict graphics drivers such as Intel Mesa

--- src/renderer/src/pass/forward.rs
+++ src/renderer/src/pass/forward.rs
@@ -19,14 +19,17 @@ pub static VERTEX_SRC: &'static [u8] = b"
     in vec3 a_Normal;
     in vec2 a_TexCoord;
 
-    out vec4 v_Position;
-    out vec3 v_Normal;
-    out vec2 v_TexCoord;
+    out VertexData {
+        vec4 v_Position;
+        vec3 v_Normal;
+        vec2 v_TexCoord;
+    } v_Out;
 
     void main() {
-        v_Position = u_Model * vec4(a_Pos, 1.0);
-        v_Normal = mat3(u_Model) * a_Normal;
-        gl_Position = u_Proj * u_View * v_Position;
+        v_Out.v_Position = u_Model * vec4(a_Pos, 1.0);
+        v_Out.v_Normal = mat3(u_Model) * a_Normal;
+        v_Out.v_TexCoord = a_TexCoord;
+        gl_Position = u_Proj * u_View * v_Out.v_Position;
     }
 ";
 
@@ -36,11 +39,16 @@ pub static FLAT_FRAGMENT_SRC: &'static [u8] = b"
     uniform sampler2D t_Ka;
     uniform sampler2D t_Kd;
 
-    in vec2 v_TexCoord;
+    in VertexData {
+        vec4 v_Position;
+        vec3 v_Normal;
+        vec2 v_TexCoord;
+    } v_In;
+
     out vec4 o_Color;
 
     void main() {
-        o_Color = texture(t_Ka, v_TexCoord);
+        o_Color = texture(t_Ka, v_In.v_TexCoord);
     }
 ";
 
@@ -65,21 +73,24 @@ pub static FRAGMENT_SRC: &'static [u8] = b"
     uniform sampler2D t_Ka;
     uniform sampler2D t_Kd;
 
-    in vec4 v_Position;
-    in vec3 v_Normal;
-    in vec2 v_TexCoord;
+    in VertexData {
+        vec4 v_Position;
+        vec3 v_Normal;
+        vec2 v_TexCoord;
+    } v_In;
+
     out vec4 o_Color;
 
     void main() {
-        vec4 kd = texture(t_Kd, v_TexCoord);
-        vec4 color = texture(t_Ka, v_TexCoord);
+        vec4 kd = texture(t_Kd, v_In.v_TexCoord);
+        vec4 color = texture(t_Ka, v_In.v_TexCoord);
         for (int i = 0; i < u_LightCount; i++) {
-            vec4 delta = light[i].center - v_Position;
+            vec4 delta = light[i].center - v_In.v_Position;
             float dist = length(delta);
             float inv_dist = 1. / dist;
             vec4 light_to_point_normal = delta * inv_dist;
             float intensity = dot(light[i].propagation.xyz, vec3(1., inv_dist, inv_dist * inv_dist));
-            color += kd * light[i].color * intensity * max(0, dot(light_to_point_normal, vec4(v_Normal, 0.)));
+            color += kd * light[i].color * intensity * max(0, dot(light_to_point_normal, vec4(v_In.v_Normal, 0.)));
         }
         o_Color = color;
     }
@@ -91,13 +102,37 @@ pub static WIREFRAME_GEOMETRY_SRC: &'static [u8] = b"
     layout(triangles) in;
     layout(line_strip, max_vertices=4) out;
 
+    in VertexData {
+        vec4 v_Position;
+        vec3 v_Normal;
+        vec2 v_TexCoord;
+    } v_In[];
+
+    out VertexData {
+        vec4 v_Position;
+        vec3 v_Normal;
+        vec2 v_TexCoord;
+    } v_Out;
+
     void main() {
+        v_Out.v_Position = v_In[0].v_Position;
+        v_Out.v_Normal = v_In[0].v_Normal;
+        v_Out.v_TexCoord = v_In[0].v_TexCoord;
         gl_Position = gl_in[0].gl_Position;
         EmitVertex();
+        v_Out.v_Position = v_In[1].v_Position;
+        v_Out.v_Normal = v_In[1].v_Normal;
+        v_Out.v_TexCoord = v_In[1].v_TexCoord;
         gl_Position = gl_in[1].gl_Position;
         EmitVertex();
+        v_Out.v_Position = v_In[2].v_Position;
+        v_Out.v_Normal = v_In[2].v_Normal;
+        v_Out.v_TexCoord = v_In[0].v_TexCoord;
         gl_Position = gl_in[2].gl_Position;
         EmitVertex();
+        v_Out.v_Position = v_In[0].v_Position;
+        v_Out.v_Normal = v_In[0].v_Normal;
+        v_Out.v_TexCoord = v_In[0].v_TexCoord;
         gl_Position = gl_in[0].gl_Position;
         EmitVertex();
         EndPrimitive();
@@ -448,4 +483,3 @@ impl<R> Pass<R> for Wireframe<R>
         }
     }
 }
-
