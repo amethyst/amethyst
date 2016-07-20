7fbd0e793c3fef0fae2860dfca07a825ac488cf8
Nikita Chashchinskii <nikita.chashinsky@yandex.ru>
2016-07-20 23:13:44 +0300

    Update examples/hello_world.rs example

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -4,26 +4,29 @@ extern crate amethyst;
 
 use amethyst::engine::{Application, State, Trans};
 use amethyst::context::{Context, Config};
+use amethyst::ecs::{Simulation, World};
 
 struct Example;
 
 impl State for Example {
-    fn on_start(&mut self, _: &mut Context) {
+    fn on_start(&mut self, _: &mut Context, _: &mut World) {
         println!("Begin!");
     }
 
-    fn update(&mut self, _: &mut Context) -> Trans {
+    fn update(&mut self, _: &mut Context, _: &mut World) -> Trans {
         println!("Hello from Amethyst!");
         Trans::Quit
     }
 
-    fn on_stop(&mut self, _: &mut Context) {
+    fn on_stop(&mut self, _: &mut Context, _: &mut World) {
         println!("End!");
     }
 }
 
 fn main() {
     let config = Config::default();
-    let mut game = Application::new(Example, config);
+    let world = World::new();
+    let simulation = Simulation::new(world, 1);
+    let mut game = Application::new(Example, simulation, config);
     game.run();
 }
