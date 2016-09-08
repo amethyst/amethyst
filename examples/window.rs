09b24305e80765df62b18785825760a68f8d6b13
Nikita Chashchinskii <nikita.chashinsky@yandex.ru>
2016-09-08 15:15:43 +0300

    Remove Broadcaster::poll method
    Reason:
    * There is no need for the user to have access to a Vec containing
      all entities for event processing, events should be polled by
      using event_storage.iter() or by iterating over (&storageA, &storageB)
      instead
    
    Other changes:
    * Remove State::handle_events and StateMachine::handle_events methods
    * Update window.rs and sphere.rs examples

--- examples/window.rs
+++ examples/window.rs
@@ -3,27 +3,11 @@ extern crate amethyst;
 use amethyst::engine::{Application, State, Trans};
 use amethyst::context::Context;
 use amethyst::config::Element;
-use amethyst::ecs::{World, Entity};
+use amethyst::ecs::{World, Join};
 
 struct Example;
 
 impl State for Example {
-    fn handle_events(&mut self, events: &[Entity], ctx: &mut Context, _: &mut World) -> Trans {
-        use amethyst::context::event::{EngineEvent, Event, VirtualKeyCode};
-        let mut trans = Trans::None;
-        let storage = ctx.broadcaster.read::<EngineEvent>();
-        for e in events {
-            let event = storage.get(*e).unwrap();
-            let event = &event.payload;
-            match *event {
-                Event::KeyboardInput(_, _, Some(VirtualKeyCode::Escape)) => trans = Trans::Quit,
-                Event::Closed => trans = Trans::Quit,
-                _ => (),
-            }
-        }
-        trans
-    }
-
     fn on_start(&mut self, ctx: &mut Context, _: &mut World) {
         use amethyst::renderer::pass::Clear;
         use amethyst::renderer::Layer;
@@ -37,6 +21,15 @@ impl State for Example {
     }
 
     fn update(&mut self, ctx: &mut Context, _: &mut World) -> Trans {
+        use amethyst::context::event::{EngineEvent, Event, VirtualKeyCode};
+        let engine_events = ctx.broadcaster.read::<EngineEvent>();
+        for engine_event in engine_events.iter() {
+            match engine_event.payload {
+                Event::KeyboardInput(_, _, Some(VirtualKeyCode::Escape)) => return Trans::Quit,
+                Event::Closed => return Trans::Quit,
+                _ => (),
+            }
+        }
         ctx.renderer.submit();
         Trans::None
     }
--- src/engine/state.rs
+++ src/engine/state.rs
@@ -33,9 +33,6 @@ pub trait State {
     /// Executed when the application returns to this game state once again.
     fn on_resume(&mut self, _ctx: &mut Context, _world: &mut World) {}
 
-    /// Executed on every frame before updating, for use in reacting to events.
-    fn handle_events(&mut self, _events: &[Entity], _ctx: &mut Context, _world: &mut World) -> Trans { Trans::None }
-
     /// Executed repeatedly at stable, predictable intervals (1/60th of a second
     /// by default).
     fn fixed_update(&mut self, _ctx: &mut Context, _world: &mut World) -> Trans { Trans::None }
@@ -82,18 +79,6 @@ impl StateMachine {
             let state = self.state_stack.last_mut().unwrap();
             state.on_start(ctx, self.planner.mut_world());
             self.running = true;
-        }
-    }
-
-    /// Passes a vector of events to the active state to handle.
-    pub fn handle_events(&mut self, events: &[Entity], ctx: &mut Context) {
-        if self.running {
-            let trans = match self.state_stack.last_mut() {
-                Some(state) => state.handle_events(events, ctx, self.planner.mut_world()),
-                None => Trans::None,
-            };
-
-            self.transition(trans, ctx);
         }
     }
 
