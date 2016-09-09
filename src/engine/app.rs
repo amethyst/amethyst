8ba3a21592bfebdd37739d52a91358f655fec515
Demur Rumed <gunkmute@gmail.com>
2016-09-09 00:30:51 +0000

    Replace time with std::time, rework Stopwatch into an enum

--- src/engine/app.rs
+++ src/engine/app.rs
@@ -1,12 +1,13 @@
 //! The core engine framework.
 
 use super::state::{State, StateMachine};
-use context::timing::{SteadyTime, Stopwatch};
+use context::timing::Stopwatch;
 use context::event::EngineEvent;
 use context::Context;
 use ecs::{Planner, World, Processor, Priority};
 use std::sync::{Arc, Mutex};
 use std::ops::DerefMut;
+use std::time::Instant;
 
 /// User-friendly facade for building games. Manages main loop.
 pub struct Application {
@@ -67,7 +68,7 @@ impl Application {
         let fixed_step = self.context.lock().unwrap().fixed_step.clone();
         let last_fixed_update = self.context.lock().unwrap().last_fixed_update.clone();
 
-        if SteadyTime::now() - last_fixed_update > fixed_step {
+        if Instant::now() - last_fixed_update > fixed_step {
             self.states.fixed_update(self.context.lock().unwrap().deref_mut());
             self.context.lock().unwrap().last_fixed_update = last_fixed_update + fixed_step;
         }
