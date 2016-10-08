ef293bd85bcf676ae969cfc2f473962be04947eb
Nikita Chashchinskii <nikita.chashinsky@yandex.ru>
2016-10-08 19:34:09 +0300

    Fix FXAA example

--- examples/05_fxaa/main.rs
+++ examples/05_fxaa/main.rs
@@ -6,26 +6,11 @@ extern crate cgmath;
 use amethyst::engine::{Application, State, Trans};
 use amethyst::context::{ContextConfig, Context};
 use amethyst::config::Element;
-use amethyst::ecs::{World, Entity};
+use amethyst::ecs::{World, Join};
 
 struct Example;
 
 impl State for Example {
-    fn handle_events(&mut self, events: &[Entity], ctx: &mut Context, _: &mut World) -> Trans {
-        use amethyst::context::event::{EngineEvent, Event, VirtualKeyCode};
-        let mut trans = Trans::None;
-        let storage = ctx.broadcaster.read::<EngineEvent>();
-        for e in events {
-            let event = storage.get(*e).unwrap();
-            match event.payload {
-                Event::KeyboardInput(_, _, Some(VirtualKeyCode::Escape)) => trans = Trans::Quit,
-                Event::Closed => trans = Trans::Quit,
-                _ => (),
-            }
-        }
-        trans
-    }
-
     fn on_start(&mut self, ctx: &mut Context, _: &mut World) {
         use amethyst::renderer::pass::{Clear, DrawShaded, FXAA, Null};
         use amethyst::renderer::{Layer, Camera, Light};
@@ -90,7 +75,17 @@ impl State for Example {
     }
 
     fn update(&mut self, ctx: &mut Context, _: &mut World) -> Trans {
-        ctx.renderer.submit();
+        // Exit if user hits Escape or closes the window
+        use amethyst::context::event::{EngineEvent, Event, VirtualKeyCode};
+        let engine_events = ctx.broadcaster.read::<EngineEvent>();
+        for engine_event in engine_events.iter() {
+            match engine_event.payload {
+                Event::KeyboardInput(_, _, Some(VirtualKeyCode::Escape)) => return Trans::Quit,
+                Event::Closed => return Trans::Quit,
+                _ => (),
+            }
+        }
+
         Trans::None
     }
 }
