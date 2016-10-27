3af7dd77efb6ec9c762fb736086a4e7b9d8bdce8
Aceeri <conmcclusk@gmail.com>
2016-10-26 23:24:28 -0700

    Revert removing public tag for crates in amethyst

--- src/lib.rs
+++ src/lib.rs
@@ -50,7 +50,7 @@ pub mod engine;
 pub mod processors;
 
 #[macro_use]
-extern crate amethyst_config as config;
-extern crate amethyst_context as context;
-extern crate amethyst_ecs as ecs;
-extern crate amethyst_renderer as renderer;
+pub extern crate amethyst_config as config;
+pub extern crate amethyst_context as context;
+pub extern crate amethyst_ecs as ecs;
+pub extern crate amethyst_renderer as renderer;
