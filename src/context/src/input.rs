0ffc063f292464f5bea968c85325efd601a1875f
bytebuddha <shadowcynical@gmail.com>
2016-11-22 08:46:02 -0600

    Added mouse functionality to InputHandler.

--- src/context/src/input.rs
+++ src/context/src/input.rs
@@ -1,10 +1,12 @@
 //! The input handler for the game engine
 
 use std::collections::HashSet;
-use event::{ElementState, EngineEvent, Event, VirtualKeyCode};
+use event::{ElementState, EngineEvent, Event, VirtualKeyCode, MouseButton };
 
 pub struct InputHandler {
     keys_down: HashSet<VirtualKeyCode>,
+    buttons_down: HashSet<MouseButton>,
+    pointer_location: (i32,i32)
 }
 
 impl InputHandler {
@@ -12,6 +14,8 @@ impl InputHandler {
     pub fn new() -> InputHandler {
         InputHandler {
             keys_down: HashSet::new(),
+            buttons_down: HashSet::new(),
+            pointer_location: (0,0)
         }
     }
 
@@ -22,6 +26,9 @@ impl InputHandler {
                 Event::KeyboardInput(ElementState::Pressed, _, Some(key_code)) => { self.keys_down.insert(key_code); }
                 Event::KeyboardInput(ElementState::Released, _, Some(key_code)) => { self.keys_down.remove(&key_code); }
                 Event::Focused(false) => self.keys_down.clear(),
+                Event::MouseMoved(x,y) => { self.pointer_location = (x,y) ; },
+                Event::MouseInput(ElementState::Pressed,button) => { self.buttons_down.insert(button); },
+                Event::MouseInput(ElementState::Released, button) => { self.buttons_down.remove(&button); },
                 _ => {}
             }
         }
@@ -30,5 +37,15 @@ impl InputHandler {
     /// Check if `key` is currently pressed
     pub fn key_down(&self, key: VirtualKeyCode) -> bool {
         self.keys_down.contains(&key)
+    }
+
+    ///Check if `button` is pressed.
+    pub fn button_down(&self, button: MouseButton) -> bool {
+        self.buttons_down.contains(&button)
+    }
+
+    ///Get pointer location.
+    pub fn pointer_location(&self) -> (i32, i32) {
+        self.pointer_location
     }
 }
