b27b64cde16c7c29385b8195168c5f57b099addd
Nikita Chashchinskii <nikita.chashinsky@yandex.ru>
2016-12-11 22:32:10 +0300

    Remove Broadcaster::poll method
    * Add export broadcaster module in world_resources/mod.rs

--- src/world_resources/broadcaster.rs
+++ src/world_resources/broadcaster.rs
@@ -9,7 +9,7 @@
 //! extern crate amethyst_ecs;
 //!
 //! use amethyst_context::broadcaster::Broadcaster;
-//! use amethyst_ecs::{Component, VecStorage};
+//! use amethyst_ecs::{Component, VecStorage, Join};
 //!
 //! struct UserComponent {
 //!     pub data: i32,
@@ -27,8 +27,8 @@
 //!         broadcaster.publish().with::<UserComponent>(user_component).build();
 //!     }
 //!     {
-//!         let storage = broadcaster.read::<UserComponent>();
-//!         for entity in broadcaster.poll() {
+//!         let user_components = broadcaster.read::<UserComponent>();
+//!         for user_component in user_components.iter() {
 //!             let user_component = storage.get(entity).unwrap();
 //!             println!("{0}", user_component.data);
 //!         }
@@ -67,13 +67,6 @@ impl Broadcaster {
         self.world.create_now()
     }
 
-    /// Return a vector containing clones of all published entities
-    pub fn poll(&self) -> Vec<Entity> {
-        let entities = self.world.entities();
-        let _entities: Vec<Entity> = entities.iter().map(|e| e.clone()).collect();
-        _entities
-    }
-
     /// Access a component storage
     pub fn read<T: Component>(&self) -> Storage<T, RwLockReadGuard<Allocator>, RwLockReadGuard<MaskedStorage<T>>> {
         self.world.read::<T>()
@@ -81,9 +74,9 @@ impl Broadcaster {
 
     /// Delete all published entities
     pub fn clean(&mut self) {
-        let entities = self.poll();
-        for entity in entities {
-            self.world.delete_now(entity);
+        for entity in self.world.entities().iter() {
+            self.world.delete_later(entity);
         }
+        self.world.maintain();
     }
 }
--- src/world_resources/mod.rs
+++ src/world_resources/mod.rs
@@ -2,8 +2,10 @@ pub mod camera;
 pub mod screen_dimensions;
 pub mod time;
 pub mod input;
+pub mod broadcaster;
 
 pub use self::camera::Camera;
 pub use self::screen_dimensions::ScreenDimensions;
 pub use self::time::Time;
 pub use self::input::InputHandler;
+pub use self::broadcaster::Broadcaster;
