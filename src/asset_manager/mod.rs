c88f0a07a4cc848122418d729d2d816d1eb6210d
torkleyy <thomas-schaller2000@gmx.de>
2017-03-08 17:35:45 +0100

    Better error handling and added load_all

--- src/asset_manager/mod.rs
+++ src/asset_manager/mod.rs
@@ -19,6 +19,7 @@ use std::collections::HashMap;
 use std::fmt::{Display, Error as FormatError, Formatter};
 use std::marker::Send;
 use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicBool, Ordering};
 
 use threadpool::ThreadPool;
 
@@ -45,12 +46,15 @@ use engine::Context;
 /// use amethyst::asset_manager::AssetLoader;
 ///
 /// let loader = AssetLoader::new();
-/// let asset_future = loader.load(my_store, "my_asset", MyFormat);
+/// loader.load(my_store, "my_asset", MyFormat);
+/// // Later
+/// let asset_option: MyAsset = loader.asset("my_asset").unwrap();
 /// ```
 pub struct AssetLoader {
     assets: HashMap<String, Box<Any>>,
     data: Arc<Mutex<Vec<AssetData>>>,
-    errors: Arc<Mutex<HashMap<String, AssetError>>>,
+    error_handler: Box<ErrorHandler>,
+    panicked: Arc<AtomicBool>,
     pool: Arc<ThreadPool>,
 }
 
@@ -73,6 +77,10 @@ pub struct AssetData {
     load_asset: LoadAsset,
 }
 
+/// The default error handler, which panics on
+/// an error.
+pub struct DefaultErrorHandler;
+
 /// The error that can occurr when trying
 /// to import asset data from a store.
 #[derive(Debug)]
@@ -89,6 +97,20 @@ pub enum AssetError {
     DataError(String),
 }
 
+/// Error handler trait.
+///
+/// You can provide such a handler for
+/// the asset loader to handle the loading errors.
+/// The default is to panic.
+pub trait ErrorHandler: Send {
+    /// Handle an error. Store "true" in `panicked` if the main
+    /// thread should panic.
+    fn handle_error(&self, name: String, e: AssetError, panicked: Arc<AtomicBool>);
+
+    /// Clones this trait object.
+    fn box_clone(&self) -> Box<ErrorHandler>;
+}
+
 impl AssetLoader {
     /// Creates a new asset loader with a cpu pool
     /// using the number of cpu cores for the number of threads.
@@ -96,20 +118,23 @@ impl AssetLoader {
         AssetLoader {
             assets: HashMap::new(),
             data: Arc::new(Mutex::new(Vec::new())),
-            errors: Arc::new(Mutex::new(HashMap::new())),
+            error_handler: Box::new(DefaultErrorHandler),
+            panicked: Arc::new(AtomicBool::new(false)),
             pool: pool,
         }
     }
 
     fn spawn(&self, sub: Submission) {
         let data = self.data.clone();
-        let errors = self.errors.clone();
-        self.pool.execute(move || Self::execute(sub, data, errors));
+        let error_handler = self.error_handler.box_clone();
+        let panicked = self.panicked.clone();
+        self.pool.execute(move || Self::execute(sub, data, error_handler, panicked));
     }
 
     fn execute(sub: Submission,
                data_vec: Arc<Mutex<Vec<AssetData>>>,
-               error_vec: Arc<Mutex<HashMap<String, AssetError>>>) {
+               error_handler: Box<ErrorHandler>,
+               panicked: Arc<AtomicBool>) {
         let Submission { name, load_data, load_asset } = sub;
 
         match load_data() {
@@ -123,10 +148,16 @@ impl AssetLoader {
                 data_vec.push(asset_data);
             }
             Err(x) => {
-                let mut errors = error_vec.lock().unwrap();
-                errors.insert(name, x);
+                error_handler.handle_error(name, x, panicked);
             }
         }
+    }
+
+    /// Set an error handler, which will be executed
+    /// when an error occured.
+    #[inline]
+    pub fn set_error_handler(&mut self, e: Box<ErrorHandler + Send>) {
+        self.error_handler = e;
     }
 
     /// Tries to retrieve an
@@ -138,39 +169,34 @@ impl AssetLoader {
     ///
     /// If `Ok(None)` is returned, the asset might not be submitted
     /// yet, has another type or it hasn't finished.
-    pub fn asset<T>(&self, name: &str) -> Result<Option<&T>, AssetError>
+    #[inline]
+    pub fn asset<T>(&self, name: &str) -> Option<&T>
         where T: Asset + 'static
     {
-        let assets = &self.assets;
-
-        match assets.get(&name.to_owned()) {
-            Some(x) => Ok(x.downcast_ref()),
-            None => {
-                let mut errors = self.errors.lock().unwrap();
-
-                match errors.remove(&name.to_owned()) {
-                    Some(x) => Err(x),
-                    None => Ok(None),
-                }
-            }
-        }
+        self.assets.get(&name.to_owned()).map(|x| x.downcast_ref().unwrap())
     }
 
     /// Load an asset on the calling thread.
     /// If it is not a very small asset, you
     /// should use `load` or `load_default`
     /// instead.
-    pub fn load_now<T, S, F>(store: &S,
-                             name: &str,
-                             format: F,
-                             context: &mut Context)
-                             -> Result<T, AssetError>
-        where T: Asset,
+    pub fn load_now<'a, T, S, F>(&'a mut self,
+                                 store: &'a S,
+                                 name: &'a str,
+                                 format: F,
+                                 context: &'a mut Context)
+                                 -> Result<T, AssetError>
+        where T: Asset + Clone + 'static,
               S: AssetStore,
               F: AssetFormat + Import<T::Data>
     {
         let data = Self::load_data::<T, S, F>(store, name, format)?;
-        T::from_data(data, context).map_err(|x| AssetError::DataError(format!("{:?}", x)))
+        let asset = T::from_data(data, context)
+            .map_err(|x| AssetError::DataError(format!("{:?}", x)));
+        let name = name.to_owned();
+        let asset = asset?;
+        self.assets.insert(name.clone(), Box::new(asset.clone()) as Box<Any>);
+        Ok(asset)
     }
 
     /// Loads just the data for some asset (blocking).
@@ -226,6 +252,20 @@ impl AssetLoader {
         self.spawn(sub);
     }
 
+    /// Load multiple assets with the same format.
+    #[inline]
+    pub fn load_all<T, S, F>(&self, store: &S, names: &[&str], format: F)
+        where T: Asset + 'static,
+              T::Data: Send + 'static,
+              T::Error: Send + 'static,
+              S: AssetStore + Clone + Send + Sync + 'static,
+              F: AssetFormat + Import<T::Data> + Clone + Send + 'static
+    {
+        for name in names {
+            self.load::<T, S, F>(store, name, format.clone());
+        }
+    }
+
     /// Loads an asset from
     /// the `DefaultStore`. You should use
     /// use this if possible because it is cross
@@ -246,8 +286,15 @@ impl AssetLoader {
 
     /// Returns the number of assets
     /// which were loaded by this asset loader.
+    #[inline]
     pub fn num_assets(&self) -> usize {
         self.assets.len()
+    }
+
+    fn panic_if_needed(&self) {
+        if self.panicked.load(Ordering::Relaxed) {
+            panic!("Propagating panic of asset loader thread to main thread");
+        }
     }
 
     /// Process a number of asset datasets
@@ -258,6 +305,8 @@ impl AssetLoader {
     /// as long as the wished frame time wasn't reached
     /// yet.
     pub fn process<F: FnMut() -> bool>(&mut self, context: &mut Context, mut as_long: F) {
+        self.panic_if_needed();
+
         let mut data_vec = self.data.lock().unwrap();
 
         loop {
@@ -275,7 +324,8 @@ impl AssetLoader {
                     self.assets.insert(name, x);
                 }
                 Err(x) => {
-                    self.errors.lock().unwrap().insert(name, x);
+                    self.error_handler.handle_error(name, x, self.panicked.clone());
+                    self.panic_if_needed()
                 }
             }
 
@@ -283,6 +333,17 @@ impl AssetLoader {
                 break;
             }
         }
+    }
+}
+
+impl ErrorHandler for DefaultErrorHandler {
+    fn handle_error(&self, name: String, error: AssetError, panicked: Arc<AtomicBool>) {
+        panicked.store(true, Ordering::Relaxed);
+        panic!("Failed to load asset \"{}\": {}", name, error);
+    }
+
+    fn box_clone(&self) -> Box<ErrorHandler> {
+        Box::new(DefaultErrorHandler) as Box<ErrorHandler>
     }
 }
 
