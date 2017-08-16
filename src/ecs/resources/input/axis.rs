d390d31c28f1b1fa163b4f4bb4f87c0b4dd3e62c
Jacob Kiesel <kieseljake@gmail.com>
2017-08-16 06:59:09 -0600

    Improve axis doc comment (#279)

--- src/ecs/resources/input/axis.rs
+++ src/ecs/resources/input/axis.rs
@@ -1,7 +1,9 @@
 use super::Button;
 
-/// Represents an axis made up of digital inputs, like W and S or A and D.
+/// Represents an emulated analogue axis made up of digital inputs, like W and S or A and D.
 /// Two of these could be analogous to a DPAD.
+///
+/// Retrieve the value of this with [axis_value](struct.InputHandler.html#method.axis_value).
 #[derive(Serialize, Deserialize)]
 pub struct Axis {
     /// Positive button, when pressed down axis value will return 1 if `neg` is not pressed down.
