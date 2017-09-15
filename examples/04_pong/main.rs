4eb1f4ef99c24ab0e97a1a30bf11fd26fc960950
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2017-09-16 00:21:28 +0300

    Rename Plank to Paddle in 04_pong

--- examples/04_pong/main.rs
+++ examples/04_pong/main.rs
@@ -50,16 +50,16 @@ enum Side {
     Right,
 }
 
-struct Plank {
+struct Paddle {
     pub position: f32,
     pub velocity: f32,
     pub dimensions: [f32; 2],
     pub side: Side,
 }
 
-impl Plank {
-    pub fn new(side: Side) -> Plank {
-        Plank {
+impl Paddle {
+    pub fn new(side: Side) -> Paddle {
+        Paddle {
             position: 0.,
             velocity: 1.,
             dimensions: [1., 1.],
@@ -68,8 +68,8 @@ impl Plank {
     }
 }
 
-impl Component for Plank {
-    type Storage = VecStorage<Plank>;
+impl Component for Paddle {
+    type Storage = VecStorage<Paddle>;
 }
 
 struct PongSystem {
@@ -95,7 +95,7 @@ impl Score {
 // Pong game system
 impl<'a> System<'a> for PongSystem {
     type SystemData = (WriteStorage<'a, Ball>,
-     WriteStorage<'a, Plank>,
+     WriteStorage<'a, Paddle>,
      WriteStorage<'a, LocalTransform>,
      Fetch<'a, Camera>,
      Fetch<'a, Time>,
@@ -333,7 +333,7 @@ impl State for Pong {
             .build();
 
         // Create a left plank entity
-        let mut plank = Plank::new(Side::Left);
+        let mut plank = Paddle::new(Side::Left);
         plank.dimensions[0] = 0.01;
         plank.dimensions[1] = 0.1;
         plank.velocity = 1.;
@@ -347,7 +347,7 @@ impl State for Pong {
             .build();
 
         // Create right plank entity
-        let mut plank = Plank::new(Side::Right);
+        let mut plank = Paddle::new(Side::Right);
         plank.dimensions[0] = 0.01;
         plank.dimensions[1] = 0.1;
         plank.velocity = 1.;
@@ -438,7 +438,7 @@ fn run() -> Result<(), amethyst::Error> {
     let mut game = Application::build(Pong)
         .unwrap()
         .register::<Ball>()
-        .register::<Plank>()
+        .register::<Paddle>()
         .with::<PongSystem>(pong, "pong_system", &[])
         .with::<TransformSystem>(TransformSystem::new(), "transform_system", &["pong_system"])
         .with_renderer(
