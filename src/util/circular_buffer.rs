b49e6619a8e4a20e96e276e2b9b5122b9cf8ff84
jojo <jojolepromain@gmail.com>
2017-09-19 14:39:19 -0400

    Formatting. Changed util::time to not use references to Duration, and only Duration.

--- src/util/circular_buffer.rs
+++ src/util/circular_buffer.rs
@@ -1,9 +1,12 @@
 //! CircularBuffer
+
 use std::collections::VecDeque;
+
 ///A CircularBuffer that drops the oldest element inserted when full.
 pub struct CircularBuffer<A> {
     queue: VecDeque<A>,
 }
+
 impl<A> CircularBuffer<A> {
     ///Creates a new CircularBuffer with fixed size
     pub fn new<B>(size: usize) -> CircularBuffer<B> {
@@ -11,6 +14,7 @@ impl<A> CircularBuffer<A> {
             queue: VecDeque::with_capacity(size),
         }
     }
+
     ///Add a value to the CircularBuffer
     ///Returns the popped value if the buffer is full
     pub fn push(&mut self, elem: A) -> Option<A> {
@@ -22,6 +26,7 @@ impl<A> CircularBuffer<A> {
         self.queue.push_back(elem);
         out
     }
+
     ///Get an immutable reference to the values inside the CircularBuffer
     pub fn queue(&self) -> &VecDeque<A> {
         &self.queue
