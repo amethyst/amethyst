ae6153ce7beee414824465723b85912d1326fa2d
Jacob Kiesel <kieseljake@gmail.com>
2017-10-07 11:12:51 -0600

    Add vergen to project

---
+++ src/vergen.rs
@@ -0,0 +1,5 @@
+//! This module is auto populated by the build script using the vergen crate.
+//! It is used to print version information at start.
+
+#![allow(dead_code)]
+include!(concat!(env!("OUT_DIR"), "/version.rs"));
