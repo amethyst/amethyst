e5af01214ba071fc4c1cd143af41a07a822c8f0b
Jacob Kiesel <kieseljake@gmail.com>
2017-12-17 11:35:32 -0700

    Export MaterialAnimation

--- amethyst_renderer/src/lib.rs
+++ amethyst_renderer/src/lib.rs
@@ -65,7 +65,7 @@ pub use formats::{build_mesh_with_combo, create_mesh_asset, create_texture_asset
 pub use input::{ElementState, Event, KeyboardInput, MouseButton, VirtualKeyCode, WindowEvent};
 pub use light::{DirectionalLight, Light, PointLight, SpotLight, SunLight};
 pub use mesh::{vertex_data, Mesh, MeshHandle, VertexBuffer};
-pub use mtl::{Material, MaterialDefaults};
+pub use mtl::{Material, MaterialAnimation, MaterialDefaults};
 pub use pass::{DrawFlat, DrawFlatSeparate, DrawPbm, DrawPbmSeparate, DrawShaded,
                DrawShadedSeparate};
 pub use pipe::{ColorBuffer, Data, DepthBuffer, DepthMode, Effect, EffectBuilder, Init, Meta,
