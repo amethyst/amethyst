894e7ea0b31f4da102c85ae65372eee83f6f1fad
Joël Lupien (Jojolepro) <jojolepromain@gmail.com>
2018-01-07 11:22:24 -0500

    Early component serialization tests

--- amethyst_network/src/components/netsync.rs
+++ amethyst_network/src/components/netsync.rs
@@ -4,3 +4,9 @@ use std::marker::PhantomData;
 pub struct NetSync<T> where T: Component{
     item:PhantomData<T>
 }
+
+#[derive(Clone, Copy, Debug, Deserialize, Serialize)]
+pub struct TestSer {
+    pub x: f32,
+    pub y: f32,
+}
