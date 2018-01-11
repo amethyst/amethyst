5d2538094b07e3f1f325507e7c06a37f99f71f96
Joël Lupien (Jojolepro) <jojolepromain@gmail.com>
2018-01-11 18:01:16 -0500

    More connection stuff, and some lifetime errors

--- amethyst_network/src/resources/net_event.rs
+++ amethyst_network/src/resources/net_event.rs
@@ -4,9 +4,26 @@ use resources::connection::*;
 
 ///The basic network events shipped with amethyst
 ///You can add more event by making a new enum and having this one has a variant of yours.
+/// #[derive(Debug,Clone,PartialEq,Serialize,Deserialize)]
+/// pub enum SampleUserEvents{
+///     Base(NetEvent),
+///     CustomEvent,
+/// }
+///
+/// impl BaseNetEvent<SampleUserEvents> for SampleUserEvents{
+///     fn base_to_custom(ev:NetEvent) -> SampleUserEvents {
+///         SampleUserEvents::Base(ev)
+///     }
+///     fn custom_to_base(ev: SampleUserEvents) -> Option<NetEvent> {
+///         match ev{
+///             SampleUserEvents::Base(e)=>Some(e),
+///             _=>None,
+///         }
+///     }
+/// }
 //TODO: Add CreateEntity,RemoveEntity,UpdateEntity
 //TODO: Example of switching the NetEvent set in the Network Systems
-#[derive(Debug,Clone,Serialize,Deserialize)]
+#[derive(Debug,Clone,PartialEq,Serialize,Deserialize)]
 pub enum NetEvent{
     Connect,
     Connected,
@@ -15,7 +32,19 @@ pub enum NetEvent{
     Disconnected{reason:String},
 }
 
-//impl Event for NetEvent{}
+impl BaseNetEvent<NetEvent> for NetEvent{
+    fn base_to_custom(ev:NetEvent) -> NetEvent {
+        ev
+    }
+    fn custom_to_base(ev: NetEvent) -> Option<NetEvent> {
+        Some(ev)
+    }
+}
+
+pub trait BaseNetEvent<T>{
+    fn base_to_custom(ev:NetEvent)->T;
+    fn custom_to_base(ev:T)->Option<NetEvent>;
+}
 
 ///Carries the source of the event. Useful for debugging, security checks, gameplay logic, etc...
 #[derive(Debug,Clone,Serialize,Deserialize)]
@@ -24,4 +53,3 @@ pub struct NetOwnedEvent<T> where T:Event{
     pub owner:NetConnection,
 }
 
-//impl<T> Event for NetOwnedEvent<T>{}
