b595722efcd246388d4d868ade6f26a536e3ef01
Sam Rhody <samuel.rhody@jhuapl.edu>
2018-01-30 15:11:06 -0500

    Adding appendix_a example

---
+++ examples/appendix_a/main.rs
@@ -0,0 +1,134 @@
+//! TODO: Rewrite for new renderer.
+
+extern crate amethyst;
+#[macro_use]
+extern crate serde_derive;
+
+mod pong;
+mod systems;
+mod bundle;
+mod audio;
+mod config;
+
+use std::time::Duration;
+
+use amethyst::Result;
+use amethyst::audio::AudioBundle;
+use amethyst::core::frame_limiter::FrameRateLimitStrategy;
+use amethyst::core::transform::TransformBundle;
+use amethyst::ecs::{Component, DenseVecStorage};
+use amethyst::input::InputBundle;
+use amethyst::prelude::*;
+use amethyst::renderer::{DisplayConfig, DrawFlat, Pipeline, PosTex, RenderBundle, Stage};
+use amethyst::ui::{DrawUi, UiBundle};
+
+use audio::Music;
+use bundle::PongBundle;
+
+const AUDIO_MUSIC: &'static [&'static str] = &[
+    "audio/Computer_Music_All-Stars_-_Wheres_My_Jetpack.ogg",
+    "audio/Computer_Music_All-Stars_-_Albatross_v2.ogg",
+];
+const AUDIO_BOUNCE: &'static str = "audio/bounce.ogg";
+const AUDIO_SCORE: &'static str = "audio/score.ogg";
+
+fn main() {
+    if let Err(e) = run() {
+        println!("Failed to execute example: {}", e);
+        ::std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
+    use pong::Pong;
+
+    let display_config_path = format!(
+        "{}/examples/pong/resources/display.ron",
+        env!("CARGO_MANIFEST_DIR")
+    );
+    let display_config = DisplayConfig::load(display_config_path);
+
+    let key_bindings_path = format!(
+        "{}/examples/pong/resources/input.ron",
+        env!("CARGO_MANIFEST_DIR")
+    );
+    
+    let config = format!(
+        "{}/examples/appendix_a/resources/config.ron",
+        env!("CARGO_MANIFEST_DIR")
+    );
+    let assets_dir = format!("{}/examples/assets/", env!("CARGO_MANIFEST_DIR"));
+    let pipe = Pipeline::build().with_stage(
+        Stage::with_backbuffer()
+            .clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
+            .with_pass(DrawFlat::<PosTex>::new())
+            .with_pass(DrawUi::new()),
+    );
+    let mut game = Application::build(assets_dir, Pong)?
+        .with_frame_limit(
+            FrameRateLimitStrategy::SleepAndYield(Duration::from_millis(2)),
+            144,
+        )
+        .with_bundle(
+            InputBundle::<String, String>::new().with_bindings_from_file(&key_bindings_path),
+        )?
+        .with_bundle(PongBundle::new(&config))?
+        .with_bundle(TransformBundle::new().with_dep(&["ball_system", "paddle_system"]))?
+        .with_bundle(AudioBundle::new(|music: &mut Music| music.music.next()))?
+        .with_bundle(UiBundle::new())?
+        .with_bundle(RenderBundle::new(pipe, Some(display_config)))?
+        .build()?;
+    game.run();
+    Ok(())
+}
+
+pub struct Ball {
+    pub velocity: [f32; 2],
+    pub radius: f32,
+}
+
+impl Component for Ball {
+    type Storage = DenseVecStorage<Self>;
+}
+
+#[derive(PartialEq, Eq)]
+pub enum Side {
+    Left,
+    Right,
+}
+
+pub struct Paddle {
+    pub velocity: f32,
+    pub side: Side,
+    pub width: f32,
+    pub height: f32,
+}
+
+impl Paddle {
+    pub fn new(side: Side) -> Paddle {
+        Paddle {
+            velocity: 1.0,
+            side: side,
+            width: 1.0,
+            height: 1.0,
+        }
+    }
+}
+
+impl Component for Paddle {
+    type Storage = DenseVecStorage<Self>;
+}
+
+pub struct ScoreBoard {
+    score_left: i32,
+    score_right: i32,
+}
+
+impl ScoreBoard {
+    pub fn new() -> ScoreBoard {
+        ScoreBoard {
+            score_left: 0,
+            score_right: 0,
+        }
+    }
+}
