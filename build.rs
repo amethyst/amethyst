ae0b2e6095958d05636d0e58ab37fcbdd44c9bf1
Théo Degioanni <moxinilian@tutanota.com>
2018-04-15 17:26:38 +0200

    Typos in parallel default behavior

--- build.rs
+++ build.rs
@@ -10,10 +10,11 @@ fn main() {
 
     use std::env;
     match (env::var("CARGO_FEATURE_PARALLEL"), env::var("CARGO_FEATURE_SERIAL")) {
-        (Some(_), Some(_)) => panic!("Can not compile in both parallel and serial mode"),
-        (None, None) => {
+        (Ok(_), Ok(_)) => panic!("Can not compile in both parallel and serial mode"),
+        (Err(_), Err(_)) => {
             #[cfg(not(target_os = "emscripten"))]
-            println!("cargo:rust-cfg:parallel");
-        }
+            println!("cargo:rustc-cfg=parallel");
+        },
+        _ => { }
     }
 }
