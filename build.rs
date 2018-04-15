0a7908e9d77059b7b651deb2a18a2f5861c3d148
Théo Degioanni <moxinilian@tutanota.com>
2018-04-15 18:57:59 +0200

    More reliable target identification

--- build.rs
+++ build.rs
@@ -8,6 +8,15 @@ fn main() {
         );
     }
 
-    #[cfg(not(target_os = "emscripten"))]
-    println!("cargo:rustc-cfg=parallel");
+    use std::env;
+    if let Ok(target) = env::var("TARGET") {
+        match target.as_ref() {
+            "asmjs-unknown-emscripten" => { },
+            "wasm32-unknown-emscripten" => { },
+            "wasm32-unknown-unknown" => { },
+            _ => println!("cargo:rustc-cfg=parallel"),
+        }
+    } else {
+        panic!("Could not identify the building target");
+    }
 }
