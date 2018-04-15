9051639edd77ff3533ee75d115c6925926573a3a
Théo Degioanni <moxinilian@tutanota.com>
2018-04-15 18:11:42 +0200

    Removed the serial feature because honestly who cares

--- build.rs
+++ build.rs
@@ -8,13 +8,6 @@ fn main() {
         );
     }
 
-    use std::env;
-    match (env::var("CARGO_FEATURE_PARALLEL"), env::var("CARGO_FEATURE_SERIAL")) {
-        (Ok(_), Ok(_)) => panic!("Can not compile in both parallel and serial mode"),
-        (Err(_), Err(_)) => {
-            #[cfg(not(target_os = "emscripten"))]
-            println!("cargo:rustc-cfg=parallel");
-        },
-        _ => { }
-    }
+    #[cfg(not(target_os = "emscripten"))]
+    println!("cargo:rustc-cfg=parallel");
 }
