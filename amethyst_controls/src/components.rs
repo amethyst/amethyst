d76fd920e9586457859f956799f6510427e3a896
mnivoliez <mathieu.nivoliez@laposte.net>
2018-05-08 21:07:59 +0200

    Issue #593
    As a arc ball mecanism was wanted, a component indicated the target and
    a system making the calculus for it was needed.
    
    Tose two elements are now reality but need testing. One way to do so is
    to provide a solid exemple of the mecanism. Also, maybe some tests could
    be implemented.

--- amethyst_controls/src/components.rs
+++ amethyst_controls/src/components.rs
@@ -1,5 +1,5 @@
-use amethyst_core::specs::prelude::Component;
-use amethyst_core::specs::storage::NullStorage;
+use amethyst_core::specs::prelude::{Component, Entity};
+use amethyst_core::specs::storage::{NullStorage, HashMapStorage};
 
 /// Add this to a camera if you want it to be a fly camera.
 /// You need to add the FlyControlBundle or the required systems for it to work.
@@ -8,4 +8,15 @@ pub struct FlyControlTag;
 
 impl Component for FlyControlTag {
     type Storage = NullStorage<FlyControlTag>;
+}
+
+pub struct ArcBallCameraTag {
+    pub target: Entity,
+}
+
+impl Component for ArcBallCameraTag {
+    // we can use HashMapStorage here because, according to the specs doc, this storage should be
+    // use when the component is use with few entity, I think there will rarely more than one
+    // camera
+    type Storage = HashMapStorage<ArcBallCameraTag>;
 }
