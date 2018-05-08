7768c833610ccdec1ce350051a5861aa8ea7af5c
p <parimal@beyond8labs.com>
2018-05-08 20:06:13 +1200

    Handle ios WindowEvent::Destroyed

--- src/app.rs
+++ src/app.rs
@@ -170,12 +170,22 @@ impl<'a, 'b> Application<'a, 'b> {
             for event in events {
                 states.handle_event(world, event.clone());
                 if !self.ignore_window_close {
-                    if let &Event::WindowEvent {
-                        event: WindowEvent::CloseRequested,
-                        ..
-                    } = &event
-                    {
-                        states.stop(world);
+                    if cfg!(target_os = "ios") {
+                        if let &Event::WindowEvent {
+                            event: WindowEvent::Destroyed,
+                            ..
+                        } = &event
+                        {
+                            states.stop(world);
+                        }
+                    } else {
+                        if let &Event::WindowEvent {
+                            event: WindowEvent::CloseRequested,
+                            ..
+                        } = &event
+                        {
+                            states.stop(world);
+                        }
                     }
                 }
             }
