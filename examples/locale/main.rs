e6fd3d88e67e7221b1eba0067c519937657ff9c6
Joël Lupien (Jojolepro) <jojolepromain@gmail.com>
2018-05-11 16:25:34 -0400

    Re-added ProgressCounter in locale example

--- examples/locale/main.rs
+++ examples/locale/main.rs
@@ -3,17 +3,18 @@
 extern crate amethyst;
 
 use amethyst::Error;
-use amethyst::assets::{AssetStorage, Handle, Loader, Processor};
+use amethyst::assets::{AssetStorage, Handle, Loader, Processor, ProgressCounter};
 use amethyst::locale::*;
 use amethyst::prelude::*;
 
 struct Example {
+    progress_counter: Option<ProgressCounter>,
     handle: Option<Handle<Locale>>,
 }
 
 impl Example {
     pub fn new() -> Self {
-        Example { handle: None }
+        Example { progress_counter: None, handle: None }
     }
 }
 
@@ -21,39 +22,43 @@ impl State for Example {
     fn on_start(&mut self, world: &mut World) {
         world.add_resource(AssetStorage::<Locale>::new());
         let loader = world.read_resource::<Loader>();
+        let mut progress_counter = ProgressCounter::default();
         self.handle = Some(loader.load(
             "locale/locale.ftl",
             LocaleFormat,
             (),
-            (),
+            &mut progress_counter,
             &world.read_resource(),
         ));
+        self.progress_counter = Some(progress_counter);
     }
 
     fn update(&mut self, world: &mut World) -> Trans {
-        let store = world.read_resource::<AssetStorage<Locale>>();
-
         // Check if the locale has been loaded.
-        // If you are doing this for multiple assets, you should be using `ProgressCounter`.
-        if let Some(locale) = self.handle.as_ref().and_then(|h| store.get(h)) {
-            println!(
-                "{}",
-                locale
-                    .context
-                    .get_message("hello")
-                    .and_then(|msg| locale.context.format(msg, None))
-                    .unwrap()
-            );
-            println!(
-                "{}",
-                locale
-                    .context
-                    .get_message("bye")
-                    .and_then(|msg| locale.context.format(msg, None))
-                    .unwrap()
-            );
-            Trans::Quit
-        } else {
+        if self.progress_counter.as_ref().unwrap().is_complete() {
+            let store = world.read_resource::<AssetStorage<Locale>>();
+            if let Some(locale) = self.handle.as_ref().and_then(|h| store.get(h)) {
+                println!(
+                    "{}",
+                    locale
+                        .context
+                        .get_message("hello")
+                        .and_then(|msg| locale.context.format(msg, None))
+                        .unwrap()
+                );
+                println!(
+                    "{}",
+                    locale
+                        .context
+                        .get_message("bye")
+                        .and_then(|msg| locale.context.format(msg, None))
+                        .unwrap()
+                );
+                Trans::Quit
+            } else {
+                Trans::None
+            }
+        }else{
             Trans::None
         }
     }
