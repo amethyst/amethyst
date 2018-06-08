aef96fafe1998da8e09b4e4069e86bef1ac5725c
Simon Rönnberg <seamonr@gmail.com>
2018-06-08 22:18:58 +0200

    feat: Add shape generator prefabs

--- amethyst_renderer/src/formats/mod.rs
+++ amethyst_renderer/src/formats/mod.rs
@@ -9,34 +9,63 @@ use amethyst_assets::{AssetPrefab, Format, PrefabData, PrefabError, ProgressCoun
 use amethyst_core::specs::prelude::Entity;
 use serde::{Deserialize, Serialize};
 
-use {Mesh, Texture};
+use shape::InternalShape;
+use {Mesh, ShapePrefab, Texture};
 
 mod mesh;
 mod mtl;
 mod texture;
 
+/// Internal mesh loading
+///
+/// ### Type parameters:
+///
+/// `V`: Vertex format to use for generated `Mesh`es, must to be one of:
+///     * `Vec<PosTex>`
+///     * `Vec<PosNormTex>`
+///     * `Vec<PosNormTangTex>`
+///     * `ComboMeshCreator`
+/// `M`: `Format` to use for loading `Mesh`es from file
+#[derive(Deserialize, Serialize)]
+pub enum MeshPrefab<V, M>
+where
+    M: Format<Mesh>,
+    M::Options: for<'a> Deserialize<'a> + Serialize,
+{
+    /// Load an asset Mesh from file
+    Asset(AssetPrefab<Mesh, M>),
+    /// Generate a Mesh from basic type
+    Shape(ShapePrefab<V>),
+}
+
 /// `PrefabData` for loading graphics, ie `Mesh` + `Material`
 ///
 /// ### Type parameters:
 ///
+/// `V`: Vertex format to use for generated `Mesh`es, must to be one of:
+///     * `Vec<PosTex>`
+///     * `Vec<PosNormTex>`
+///     * `Vec<PosNormTangTex>`
+///     * `ComboMeshCreator`
 /// - `M`: `Format` to use for loading `Mesh`es from file
 /// - `T`: `Format` to use for loading `Texture`s from file
 #[derive(Deserialize, Serialize)]
-pub struct GraphicsPrefab<M, T>
+pub struct GraphicsPrefab<V, M = ObjFormat, T = TextureFormat>
 where
     M: Format<Mesh>,
     M::Options: for<'a> Deserialize<'a> + Serialize,
     T: Format<Texture, Options = TextureMetadata>,
 {
-    mesh: AssetPrefab<Mesh, M>,
+    mesh: MeshPrefab<V, M>,
     material: MaterialPrefab<T>,
 }
 
-impl<'a, M, T> PrefabData<'a> for GraphicsPrefab<M, T>
+impl<'a, V, M, T> PrefabData<'a> for GraphicsPrefab<V, M, T>
 where
     M: Format<Mesh> + Clone,
     M::Options: Clone + for<'b> Deserialize<'b> + Serialize,
     T: Format<Texture, Options = TextureMetadata> + Sync + Clone,
+    V: From<InternalShape> + Into<MeshData>,
 {
     type SystemData = (
         <AssetPrefab<Mesh, M> as PrefabData<'a>>::SystemData,
@@ -50,7 +79,14 @@ where
         system_data: &mut <Self as PrefabData>::SystemData,
         entities: &[Entity],
     ) -> Result<(), PrefabError> {
-        self.mesh.load_prefab(entity, &mut system_data.0, entities)?;
+        match self.mesh {
+            MeshPrefab::Asset(ref m) => {
+                m.load_prefab(entity, &mut system_data.0, entities)?;
+            }
+            MeshPrefab::Shape(ref s) => {
+                s.load_prefab(entity, &mut system_data.0, entities)?;
+            }
+        }
         self.material
             .load_prefab(entity, &mut system_data.1, entities)?;
         Ok(())
@@ -62,7 +98,10 @@ where
         system_data: &mut Self::SystemData,
     ) -> Result<bool, PrefabError> {
         let mut ret = false;
-        if self.mesh.trigger_sub_loading(progress, &mut system_data.0)? {
+        if match self.mesh {
+            MeshPrefab::Asset(ref mut m) => m.trigger_sub_loading(progress, &mut system_data.0)?,
+            MeshPrefab::Shape(ref mut s) => s.trigger_sub_loading(progress, &mut system_data.0)?,
+        } {
             ret = true;
         }
         if self.material
