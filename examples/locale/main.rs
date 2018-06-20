37996b61ab56b256e033ff6ec8352d1f37d14525
Jojolepro <jojolepromain@gmail.com>
2018-06-20 10:55:03 -0400

    Added french example locale. Now using world.exec. Fixed missing data.update.

--- examples/locale/main.rs
+++ examples/locale/main.rs
@@ -4,60 +4,62 @@ extern crate amethyst;
 
 use amethyst::Error;
 use amethyst::assets::{AssetStorage, Handle, Loader, Processor, ProgressCounter};
+use amethyst::ecs::{Read,ReadExpect};
 use amethyst::locale::*;
 use amethyst::prelude::*;
 
 struct Example {
     progress_counter: Option<ProgressCounter>,
-    handle: Option<Handle<Locale>>,
+    handle_en: Option<Handle<Locale>>,
+    handle_fr: Option<Handle<Locale>>,
 }
 
 impl Example {
     pub fn new() -> Self {
-        Example { progress_counter: None, handle: None }
+        Example { progress_counter: None, handle_en: None, handle_fr: None }
     }
 }
 
 impl<'a,'b> State<GameData<'a, 'b>> for Example {
     fn on_start(&mut self, data: StateData<GameData>) {
         data.world.add_resource(AssetStorage::<Locale>::new());
-        let loader = data.world.read_resource::<Loader>();
         let mut progress_counter = ProgressCounter::default();
-        self.handle = Some(loader.load(
-            "locale/locale.ftl",
-            LocaleFormat,
-            (),
-            &mut progress_counter,
-            &data.world.read_resource(),
-        ));
+        self.handle_en = Some(data.world.exec(|(loader,storage): (ReadExpect<Loader>,Read<AssetStorage<Locale>>)| {
+            loader.load("locale/locale_en.ftl", LocaleFormat, (), &mut progress_counter,&storage)
+        }));
+        self.handle_fr = Some(data.world.exec(|(loader,storage): (ReadExpect<Loader>,Read<AssetStorage<Locale>>)| {
+            loader.load("locale/locale_fr.ftl", LocaleFormat, (), &mut progress_counter,&storage)
+        }));
         self.progress_counter = Some(progress_counter);
     }
 
     fn update(&mut self, data: StateData<GameData>) -> Trans<GameData<'a, 'b>> {
+        data.data.update(&data.world);
+        
         // Check if the locale has been loaded.
         if self.progress_counter.as_ref().unwrap().is_complete() {
             let store = data.world.read_resource::<AssetStorage<Locale>>();
-            if let Some(locale) = self.handle.as_ref().and_then(|h| store.get(h)) {
-                println!(
-                    "{}",
-                    locale
-                        .context
-                        .get_message("hello")
-                        .and_then(|msg| locale.context.format(msg, None))
-                        .unwrap()
-                );
-                println!(
-                    "{}",
-                    locale
-                        .context
-                        .get_message("bye")
-                        .and_then(|msg| locale.context.format(msg, None))
-                        .unwrap()
-                );
-                Trans::Quit
-            } else {
-                Trans::None
+            for h in [&self.handle_en, &self.handle_fr].iter(){
+                if let Some(locale) = h.as_ref().and_then(|h| store.get(h)) {
+                    println!(
+                        "{}",
+                        locale
+                            .context
+                            .get_message("hello")
+                            .and_then(|msg| locale.context.format(msg, None))
+                            .unwrap()
+                    );
+                    println!(
+                        "{}",
+                        locale
+                            .context
+                            .get_message("bye")
+                            .and_then(|msg| locale.context.format(msg, None))
+                            .unwrap()
+                    );
+                }
             }
+            Trans::Quit
         }else{
             Trans::None
         }
