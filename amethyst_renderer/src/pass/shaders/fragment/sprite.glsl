20cfca657ec4b8af00fbe70fc023642968c571fb
Azriel Hoh <azriel91@gmail.com>
2018-08-01 08:33:08 +1200

    Calculate texture coordinates in vertex shader.
    This way it's calculated 6 times instead of `w * h` times.

--- amethyst_renderer/src/pass/shaders/fragment/sprite.glsl
+++ amethyst_renderer/src/pass/shaders/fragment/sprite.glsl
@@ -4,25 +4,10 @@
 
 uniform sampler2D albedo;
 
-layout (std140) uniform AlbedoOffset {
-    vec2 u_offset;
-    vec2 v_offset;
-} albedo_offset;
-
 in vec2 tex_uv;
 
 out vec4 color;
 
-// coord = 0.0 to 1.0 texture coordinate
-float texture_coord(float coord, vec2 offset) {
-    return offset.x + coord * (offset.y - offset.x);
-}
-
-// coords = 0.0 to 1.0 texture coordinates
-vec2 texture_coords(vec2 coords, vec2 u, vec2 v) {
-    return vec2(texture_coord(coords.x, u), texture_coord(coords.y, v));
-}
-
 void main() {
-    color = texture(albedo, texture_coords(tex_uv, albedo_offset.u_offset, albedo_offset.v_offset));
+    color = texture(albedo, tex_uv);
 }
--- amethyst_renderer/src/pass/shaders/vertex/sprite.glsl
+++ amethyst_renderer/src/pass/shaders/vertex/sprite.glsl
@@ -13,6 +13,11 @@ layout (std140) uniform SpriteArgs {
     uniform vec2 offsets;
 };
 
+layout (std140) uniform AlbedoOffset {
+    vec2 u_offset;
+    vec2 v_offset;
+} albedo_offset;
+
 out vec2 tex_uv;
 out vec2 uv;
 
@@ -29,9 +34,21 @@ const vec2 positions[6] = vec2[](
     vec2(0.0, 0.0)  // Left bottom
 );
 
+// coord = 0.0 to 1.0 texture coordinate
+float texture_coord(float coord, vec2 offset) {
+    return offset.x + coord * (offset.y - offset.x);
+}
+
+// coords = 0.0 to 1.0 texture coordinates
+vec2 texture_coords(vec2 coords, vec2 u, vec2 v) {
+    return vec2(texture_coord(coords.x, u), texture_coord(coords.y, v));
+}
+
 void main() {
-    tex_uv = positions[gl_VertexID];
-    uv = tex_uv * sprite_dimensions;
+    vec2 position = positions[gl_VertexID];
+
+    uv = position * sprite_dimensions;
+    tex_uv = texture_coords(position, albedo_offset.u_offset, albedo_offset.v_offset);
 
     vec4 vertex = vec4(uv, 0.0, 1.0);
     gl_Position = proj * view * model * vertex;
