9ead091325ec0fe60712a6ab2169aaf2b93f2a48
Azriel Hoh <azriel91@gmail.com>
2018-08-13 16:25:21 +1200

    Added `sprites_ordered` example that demonstrates sprite visibility.
    Issue #829

---
+++ examples/sprites_ordered/png_loader.rs
@@ -0,0 +1,23 @@
+use amethyst::assets::{AssetStorage, Loader};
+use amethyst::prelude::*;
+use amethyst::renderer::{PngFormat, Texture, TextureHandle};
+
+/// Returns a `TextureHandle` to the image.
+///
+/// # Parameters
+///
+/// * `name`: Path to the sprite sheet.
+/// * `world`: `World` that stores resources.
+pub fn load<N>(name: N, world: &World) -> TextureHandle
+where
+    N: Into<String>,
+{
+    let loader = world.read_resource::<Loader>();
+    loader.load(
+        name,
+        PngFormat,
+        Default::default(),
+        (),
+        &world.read_resource::<AssetStorage<Texture>>(),
+    )
+}
