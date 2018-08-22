2961fa5df46e75f7fe65d230af2d62389df418fd
msiglreith <m.siglreith@gmail.com>
2018-08-22 23:08:10 +0200

    Implement sprite batching by packing calls with same state

--- amethyst_renderer/src/pass/sprite/interleaved.rs
+++ amethyst_renderer/src/pass/sprite/interleaved.rs
@@ -4,29 +4,30 @@ use amethyst_assets::{AssetStorage, Handle};
 use amethyst_core::cgmath::Vector4;
 use amethyst_core::specs::prelude::{Join, Read, ReadStorage};
 use amethyst_core::transform::GlobalTransform;
+
 use gfx_core::state::{Blend, ColorMask};
 use glsl_layout::Uniform;
 
 use super::*;
 use cam::{ActiveCamera, Camera};
 use error::Result;
+use gfx::pso::buffer::ElemStride;
 use mtl::MaterialTextureSet;
-use pass::util::{
-    add_texture, get_camera, set_sprite_args, set_view_args, setup_textures, SpriteArgs,
-    TextureOffsetPod, ViewArgs,
-};
+use pass::util::{add_texture, get_camera, set_view_args, setup_textures, ViewArgs};
 use pipe::pass::{Pass, PassData};
 use pipe::{DepthMode, Effect, NewEffect};
 use sprite::{SpriteRender, SpriteSheet};
 use sprite_visibility::SpriteVisibility;
 use tex::Texture;
 use types::{Encoder, Factory, Slice};
+use vertex::{Attributes, Query, VertexFormat};
 
 /// Draws sprites on a 2D quad.
-#[derive(Derivative, Clone, Debug, PartialEq)]
+#[derive(Derivative, Clone, Debug)]
 #[derivative(Default(bound = "Self: Pass"))]
 pub struct DrawSprite {
     transparency: Option<(ColorMask, Blend, Option<DepthMode>)>,
+    batch: SpriteBatch,
 }
 
 impl DrawSprite
@@ -48,6 +49,10 @@ where
         self.transparency = Some((mask, blend, depth));
         self
     }
+
+    fn attributes() -> Attributes<'static> {
+        <SpriteInstance as Query<(DirX, DirY, Pos, OffsetU, OffsetV)>>::QUERIED_ATTRIBUTES
+    }
 }
 
 impl<'a> PassData<'a> for DrawSprite {
@@ -65,8 +70,6 @@ impl<'a> PassData<'a> for DrawSprite {
 
 impl Pass for DrawSprite {
     fn compile(&mut self, effect: NewEffect) -> Result<Effect> {
-        use gfx::format::{ChannelType, Format, SurfaceType};
-        use gfx::pso::buffer::Element;
         use std::mem;
 
         let mut builder = effect.simple(VERT_SRC, FRAG_SRC);
@@ -76,40 +79,7 @@ impl Pass for DrawSprite {
                 mem::size_of::<<ViewArgs as Uniform>::Std140>(),
                 1,
             )
-            .with_raw_vertex_buffer(
-                &[
-                    (
-                        "size",
-                        Element {
-                            offset: 0,
-                            format: Format(SurfaceType::R32_G32, ChannelType::Float),
-                        },
-                    ),
-                    (
-                        "offsets",
-                        Element {
-                            offset: 8,
-                            format: Format(SurfaceType::R32_G32, ChannelType::Float),
-                        },
-                    ),
-                    (
-                        "u_offset",
-                        Element {
-                            offset: 16,
-                            format: Format(SurfaceType::R32_G32, ChannelType::Float),
-                        },
-                    ),
-                    (
-                        "v_offset",
-                        Element {
-                            offset: 24,
-                            format: Format(SurfaceType::R32_G32, ChannelType::Float),
-                        },
-                    ),
-                ],
-                32,
-                1,
-            );
+            .with_raw_vertex_buffer(Self::attributes(), SpriteInstance::size() as ElemStride, 1);
         setup_textures(&mut builder, &TEXTURES);
         match self.transparency {
             Some((mask, blend, depth)) => builder.with_blended_output("color", mask, blend, depth),
@@ -136,47 +106,49 @@ impl Pass for DrawSprite {
     ) {
         let camera = get_camera(active, &camera, &global);
 
-        let mut batch = SpriteBatch::new();
         match visibility {
             None => {
                 for (sprite_render, global) in (&sprite_render, &global).join() {
-                    batch.add_sprite(
+                    self.batch.add_sprite(
                         sprite_render,
                         Some(global),
                         &sprite_sheet_storage,
                         &material_texture_set,
+                        &tex_storage,
                     );
                 }
-                batch.sort();
+                self.batch.sort();
             }
             Some(ref visibility) => {
                 for (sprite_render, global, _) in
                     (&sprite_render, &global, &visibility.visible_unordered).join()
                 {
-                    batch.add_sprite(
+                    self.batch.add_sprite(
                         sprite_render,
                         Some(global),
                         &sprite_sheet_storage,
                         &material_texture_set,
+                        &tex_storage,
                     );
                 }
 
                 // We are free to optimize the order of the opaque sprites.
-                batch.sort();
+                self.batch.sort();
 
                 for entity in &visibility.visible_ordered {
                     if let Some(sprite_render) = sprite_render.get(*entity) {
-                        batch.add_sprite(
+                        self.batch.add_sprite(
                             sprite_render,
                             global.get(*entity),
                             &sprite_sheet_storage,
                             &material_texture_set,
+                            &tex_storage,
                         );
                     }
                 }
             }
         }
-        batch.encode(
+        self.batch.encode(
             encoder,
             &mut factory,
             effect,
@@ -184,58 +156,67 @@ impl Pass for DrawSprite {
             &sprite_sheet_storage,
             &tex_storage,
         );
+        self.batch.reset();
     }
 }
 
+#[derive(Clone, Debug)]
 struct SpriteDrawData {
     texture: Handle<Texture>,
     render: SpriteRender,
     transform: GlobalTransform,
 }
 
+#[derive(Clone, Default, Debug)]
 struct SpriteBatch {
     sprites: Vec<SpriteDrawData>,
 }
 
 impl SpriteBatch {
-    pub fn new() -> Self {
-        SpriteBatch {
-            sprites: Vec::new(),
-        }
-    }
-
     pub fn add_sprite(
         &mut self,
         sprite_render: &SpriteRender,
         global: Option<&GlobalTransform>,
         sprite_sheet_storage: &AssetStorage<SpriteSheet>,
         material_texture_set: &MaterialTextureSet,
+        tex_storage: &AssetStorage<Texture>,
     ) {
         if global.is_none() {
             return;
         }
 
-        let sprite_sheet = sprite_sheet_storage.get(&sprite_render.sprite_sheet);
-        if sprite_sheet.is_none() {
-            warn!(
-                "Sprite sheet not loaded for sprite_render: `{:?}`.",
-                sprite_render
-            );
-            return;
-        }
-        let sprite_sheet = sprite_sheet.unwrap();
+        let texture_handle = match sprite_sheet_storage.get(&sprite_render.sprite_sheet) {
+            Some(sprite_sheet) => match material_texture_set.handle(sprite_sheet.texture_id) {
+                Some(texture_handle) => {
+                    if tex_storage.get(&texture_handle).is_none() {
+                        warn!(
+                            "Texture not loaded for texture id: `{}`.",
+                            sprite_sheet.texture_id
+                        );
+                        return;
+                    }
 
-        let texture_handle = material_texture_set.handle(sprite_sheet.texture_id);
-        if texture_handle.is_none() {
-            warn!(
-                "Texture handle not found for texture id: `{}`.",
-                sprite_sheet.texture_id
-            );
-            return;
-        }
+                    texture_handle
+                }
+                None => {
+                    warn!(
+                        "Texture handle not found for texture id: `{}`.",
+                        sprite_sheet.texture_id
+                    );
+                    return;
+                }
+            },
+            None => {
+                warn!(
+                    "Sprite sheet not loaded for sprite_render: `{:?}`.",
+                    sprite_render
+                );
+                return;
+            }
+        };
 
         self.sprites.push(SpriteDrawData {
-            texture: texture_handle.unwrap(),
+            texture: texture_handle,
             render: sprite_render.clone(),
             transform: global.unwrap().clone(),
         });
@@ -268,27 +249,19 @@ impl SpriteBatch {
         // Sprite vertex shader
         set_view_args(effect, encoder, camera);
 
-        for sprite in &self.sprites {
-            let sprite_sheet = sprite_sheet_storage.get(&sprite.render.sprite_sheet);
-            if sprite_sheet.is_none() {
-                warn!(
-                    "Sprite sheet not loaded for sprite_render: `{:?}`.",
-                    sprite.render
-                );
-                return;
-            }
-            let sprite_sheet = sprite_sheet.unwrap();
+        let mut instance_data = Vec::<f32>::new();
+        let mut num_instances = 0;
+        let num_sprites = self.sprites.len();
 
-            let texture = tex_storage.get(&sprite.texture);
-            if texture.is_none() {
-                warn!(
-                    "Texture not loaded for texture id: `{}`.",
-                    sprite_sheet.texture_id
-                );
-                return;
-            }
-            add_texture(effect, texture.unwrap());
+        for (i, sprite) in self.sprites.iter().enumerate() {
+            // `unwrap` checked when collecting the sprites.
+            let sprite_sheet = sprite_sheet_storage
+                .get(&sprite.render.sprite_sheet)
+                .unwrap();
 
+            let texture = tex_storage.get(&sprite.texture).unwrap();
+
+            // Append sprite to instance data.
             let sprite_data = &sprite_sheet.sprites[sprite.render.sprite_number];
 
             let tex_coords = &sprite_data.tex_coords;
@@ -305,36 +278,55 @@ impl SpriteBatch {
 
             let transform = &sprite.transform.0;
 
-            let size = transform * Vector4::new(sprite_data.width, sprite_data.height, 0.0, 0.0);
-            let offset =
+            let dir_x = transform.x * sprite_data.width;
+            let dir_y = transform.y * sprite_data.height;
+            let pos =
                 transform * Vector4::new(sprite_data.offsets[0], sprite_data.offsets[1], 0.0, 1.0);
 
-            let vbuf = factory
-                .create_buffer_immutable(
-                    &[
-                        size.x, size.y, offset.x, offset.y, uv_left, uv_right, uv_bottom, uv_top,
-                    ],
-                    buffer::Role::Vertex,
-                    Bind::empty(),
-                )
-                .unwrap();
+            instance_data.extend(&[
+                dir_x.x, dir_x.y, dir_y.x, dir_y.y, pos.x, pos.y, uv_left, uv_right, uv_bottom,
+                uv_top,
+            ]);
+            num_instances += 1;
 
-            effect.data.vertex_bufs.push(vbuf.raw().clone());
-            effect.data.vertex_bufs.push(vbuf.raw().clone());
-            effect.data.vertex_bufs.push(vbuf.raw().clone());
-            effect.data.vertex_bufs.push(vbuf.raw().clone());
+            // Need to flush outstanding draw calls due to state switch (texture).
+            //
+            // 1. We are at the last sprite and want to submit all pending work.
+            // 2. The next sprite will use a different texture triggering a flush.
+            let need_flush =
+                i >= num_sprites - 1 || self.sprites[i + 1].texture.id() != sprite.texture.id();
 
-            effect.draw(
-                &Slice {
-                    start: 0,
-                    end: 6,
-                    base_vertex: 0,
-                    instances: None,
-                    buffer: Default::default(),
-                },
-                encoder,
-            );
-            effect.clear();
+            if need_flush {
+                add_texture(effect, texture);
+
+                let vbuf = factory
+                    .create_buffer_immutable(&instance_data, buffer::Role::Vertex, Bind::empty())
+                    .unwrap();
+
+                for _ in DrawSprite::attributes() {
+                    effect.data.vertex_bufs.push(vbuf.raw().clone());
+                }
+
+                effect.draw(
+                    &Slice {
+                        start: 0,
+                        end: 6,
+                        base_vertex: 0,
+                        instances: Some((num_instances, 0)),
+                        buffer: Default::default(),
+                    },
+                    encoder,
+                );
+
+                effect.clear();
+
+                num_instances = 0;
+                instance_data.clear();
+            }
         }
+    }
+
+    pub fn reset(&mut self) {
+        self.sprites.clear();
     }
 }
