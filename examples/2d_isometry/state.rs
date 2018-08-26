53eb8db8d12c7a6617ca3c8e4fc3f651d1b2e774
Théo Degioanni <moxinilian@tutanota.com>
2018-08-26 09:55:19 +0200

    Updated to SimpleState

--- examples/2d_isometry/state.rs
+++ examples/2d_isometry/state.rs
@@ -1,6 +1,4 @@
-use amethyst::input::{is_close_requested, is_key_down};
 use amethyst::prelude::*;
-use amethyst::renderer::{Event, VirtualKeyCode};
 
 use camera::initialise_camera;
 use map::initialise_map;
@@ -9,7 +7,7 @@ use cars::{initialise_cars, load_cars_sprite_sheet};
 
 pub struct IsometryState;
 
-impl<'a, 'b> State<GameData<'a, 'b>> for IsometryState {
+impl<'a, 'b> SimpleState<'a, 'b> for IsometryState {
     fn on_start(&mut self, data: StateData<GameData>) {
         let world = data.world;
 
@@ -19,18 +17,5 @@ impl<'a, 'b> State<GameData<'a, 'b>> for IsometryState {
         initialise_camera(world);
         initialise_map(world, sprite_sheet);
         initialise_cars(world, cars_sprite_sheet);
-    }
-
-    fn handle_event(&mut self, _: StateData<GameData>, event: Event) -> Trans<GameData<'a, 'b>> {
-        if is_close_requested(&event) || is_key_down(&event, VirtualKeyCode::Escape) {
-            Trans::Quit
-        } else {
-            Trans::None
-        }
-    }
-
-    fn update(&mut self, data: StateData<GameData>) -> Trans<GameData<'a, 'b>> {
-        data.data.update(&data.world);
-        Trans::None
     }
 }
