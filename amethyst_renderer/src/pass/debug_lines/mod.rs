6ad6358516cb22b46263482fb4ab186d1df5bdef
Karll <karllhenning64@gmail.com>
2018-08-31 11:52:45 -0300

    Removed unnecessary information derived from base code for the debug lines pass

--- amethyst_renderer/src/pass/debug_lines/mod.rs
+++ amethyst_renderer/src/pass/debug_lines/mod.rs
@@ -2,10 +2,6 @@ pub use self::separate::DrawDebugLinesSeparate;
 
 mod separate;
 
-use pass::util::TextureType;
-
 static VERT_SRC: &[u8] = include_bytes!("../shaders/vertex/debug_lines.glsl");
 static GEOM_SRC: &[u8] = include_bytes!("../shaders/geometry/debug_lines.glsl");
 static FRAG_SRC: &[u8] = include_bytes!("../shaders/fragment/debug_lines.glsl");
-
-static TEXTURES: [TextureType; 0] = [];
