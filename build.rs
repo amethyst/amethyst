f1ec75c16b21c46dfc09ae2e19a3f789b019a1ee
Trangar <Victor 'Trangar' Koenders>
2018-09-01 15:54:45 +0200

    Replaced match by unwrap_or_else

--- build.rs
+++ build.rs
@@ -3,15 +3,12 @@ extern crate vergen;
 use vergen::{ConstantsFlags, Vergen};
 
 fn main() {
-    let vergen = match Vergen::new(ConstantsFlags::all()) {
-        Ok(v) => v,
-        Err(err) => {
-            panic!(
-                "Vergen crate failed to generate version information! {:?}",
-                err
-            );
-        }
-    };
+    let vergen = Vergen::new(ConstantsFlags::all()).unwrap_or_else(|e| {
+        panic!(
+            "Vergen crate failed to generate version information! {:?}",
+            e
+        );
+    });
 
     for (k, v) in vergen.build_info() {
         println!("cargo:rustc-env={}={}", k.name(), v);
