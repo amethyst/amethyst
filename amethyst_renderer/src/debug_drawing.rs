7895c1586e9f2e71edd46e713fe460fd0a42ec6b
Karll <karllhenning64@gmail.com>
2018-09-12 20:14:38 -0300

    Added debug lines as component and resource; Created key bindings config file

--- amethyst_renderer/src/debug_drawing.rs
+++ amethyst_renderer/src/debug_drawing.rs
@@ -1,33 +1,47 @@
-use amethyst_core::specs::{
-    Component, DenseVecStorage, Entity, HashMapStorage, Write, WriteStorage,
-};
+use amethyst_core::specs::{Component, DenseVecStorage};
 use color::Rgba;
 use vertex::PosColorNorm;
+
+/// Debug lines are stored as a start position, a direction and a color.
+///
+/// Storing a direction instead of an end position may not be intuitive,
+/// but is similar to other 'VertexFormat's.
+pub type DebugLine = PosColorNorm;
 
 /// Resource that stores debug lines to be rendered in DebugLinesPass draw pass
 #[derive(Debug)]
 pub struct DebugLines {
-    /// Lines to be rendered this frame
-    pub lines: Vec<PosColorNorm>,
+    /// Lines to be rendered
+    pub lines: Vec<DebugLine>,
+}
+
+impl Component for DebugLines {
+    type Storage = DenseVecStorage<Self>;
+}
+
+impl Default for DebugLines {
+    fn default() -> Self {
+        DebugLines::new()
+    }
 }
 
 impl DebugLines {
     /// Creates a new screen dimensions object with the given width and height.
     pub fn new() -> DebugLines {
         DebugLines {
-            lines: Vec::<PosColorNorm>::new(),
+            lines: Vec::<DebugLine>::new(),
         }
     }
 
     /// Builder method to pre-allocate a number of line.
     pub fn with_capacity(mut self, capacity: usize) -> Self {
-        self.lines = Vec::<PosColorNorm>::with_capacity(capacity);
+        self.lines = Vec::<DebugLine>::with_capacity(capacity);
         self
     }
 
     /// Adds a line to be rendered by giving a position and a direction.
     pub fn add_as_direction(&mut self, position: [f32; 3], direction: [f32; 3], color: Rgba) {
-        let vertex = PosColorNorm {
+        let vertex = DebugLine {
             position: position,
             color: color.into(),
             normal: direction,
@@ -38,7 +52,7 @@ impl DebugLines {
 
     /// Adds a line to be rendered by giving a start and an end position.
     pub fn add_as_line(&mut self, start: [f32; 3], end: [f32; 3], color: Rgba) {
-        let vertex = PosColorNorm {
+        let vertex = DebugLine {
             position: start,
             color: color.into(),
             normal: [end[0] - start[0], end[1] - start[1], end[2] - start[2]],
@@ -46,8 +60,4 @@ impl DebugLines {
 
         self.lines.push(vertex);
     }
-}
-
-impl Component for DebugLines {
-    type Storage = DenseVecStorage<Self>;
 }
