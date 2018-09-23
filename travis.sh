0e5b769553b3d7f67c37900a8450173760852887
Théo Degioanni <moxinilian@tutanota.com>
2018-09-23 14:50:37 +0200

    Make Travis test the book
    Move the travis script to bash file and use prebuilt mdbook
    
    Fix typo
    
    Make book testing Linux-only
    
    Fix typo
    
    Make Travis abort when tests fail
    
    Line break
    
    Test build error
    
    Test test failure
    
    Good to go
    
    Add testing to prefab chapter
    
    Make Travis test the book
    
    Move the travis script to bash file and use prebuilt mdbook
    
    Fix typo
    
    Make book testing Linux-only
    
    Fix typo
    
    Make Travis abort when tests fail
    
    Line break
    
    Test build error
    
    Test test failure
    
    Good to go
    
    Forgot a test
    
    Add tests to SystemData in book

---
+++ travis.sh
@@ -0,0 +1,21 @@
+MDBOOK_RELEASE="v0.2.1/mdbook-v0.2.1-x86_64-unknown-linux-gnu.tar.gz"
+
+echo "Build and test without profiler"
+cargo test --all -v || exit 1
+
+if [ ${TRAVIS_OS_NAME} = "linux" ]
+then
+    echo "Install mdbook"
+    curl -L -o mdbook.tar.gz https://github.com/rust-lang-nursery/mdBook/releases/download/${MDBOOK_RELEASE}
+    tar -xvf mdbook.tar.gz -C ./
+    rm mdbook.tar.gz
+
+    echo "Build all the examples in the book"
+    ./mdbook test book -L target/debug/deps || exit 2
+fi
+
+echo "Build and test with profiler"
+cargo test --all --features profiler -v || exit 3
+
+echo "Build and test with sdl_controller"
+cargo test --all --features sdl_controller -v || exit 4
