d97b77a75c31bfdc7ebd55044e3e6120259f67e4
PLR <PLR>
2018-10-15 00:49:56 -0400

    cargo build again, but on stable

--- amethyst_physics/src/lib.rs
+++ amethyst_physics/src/lib.rs
@@ -1,14 +1,11 @@
 #[macro_use]
 extern crate log;
 
+extern crate amethyst_core;
+extern crate amethyst_input;
+extern crate amethyst_renderer;
+extern crate amethyst_ui;
+
 extern crate collision;
 
 pub mod picking;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
