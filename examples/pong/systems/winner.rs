3629a251d8aafc723a0d140071f22de441c12ec1
Théo Degioanni <moxinilian@tutanota.com>
2018-10-22 22:11:42 +0200

    Fix the pong scoreboard

--- examples/pong/systems/winner.rs
+++ examples/pong/systems/winner.rs
@@ -45,14 +45,16 @@ impl<'s> System<'s> for WinnerSystem {
 
             let did_hit = if ball_x <= ball.radius {
                 // Right player scored on the left side.
-                score_board.score_right += 1;
+                // We top the score at 999 to avoid text overlap.
+                score_board.score_right = (score_board.score_right + 1).min(999);
                 if let Some(text) = text.get_mut(score_text.p2_score) {
                     text.text = score_board.score_right.to_string();
                 }
                 true
             } else if ball_x >= ARENA_WIDTH - ball.radius {
                 // Left player scored on the right side.
-                score_board.score_left += 1;
+                // We top the score at 999 to avoid text overlap.
+                score_board.score_left = (score_board.score_left + 1).min(999);
                 if let Some(text) = text.get_mut(score_text.p1_score) {
                     text.text = score_board.score_left.to_string();
                 }
