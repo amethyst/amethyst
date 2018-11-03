ce7b69f187d6c245f0647f919f9796b38111d8cb
Douman <douman@gmx.se>
2018-11-03 23:10:14 +0300

    Correct rustfmt formatting

--- amethyst_ui/src/font/default.rs
+++ amethyst_ui/src/font/default.rs
@@ -27,10 +27,10 @@ pub fn get_default_font(loader: &Loader, storage: &AssetStorage<FontAsset>) -> F
 
                     if let Some(format) = format {
                         match fs::read(&path) {
-                        Ok(bytes) => match format.import(bytes, ()) {
-                            Ok(data) => return loader.load_from_data(data, (), storage),
-                            Err(err) => warn!("System font at '{}' cannot be loaded. Fallback to default. Error: {}", path.display(), err),
-                        },
+                            Ok(bytes) => match format.import(bytes, ()) {
+                                Ok(data) => return loader.load_from_data(data, (), storage),
+                                Err(err) => warn!("System font at '{}' cannot be loaded. Fallback to default. Error: {}", path.display(), err),
+                            },
                         Err(err) => warn!("System font at '{}' is not available for use. Fallback to default. Error: {}", path.display(), err)
                     }
                     }
