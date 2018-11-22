7f4ea83c294d20ab7defb8097ae8e72c86fa7df4
Joël Lupien (Jojolepro) <jojolepromain@gmail.com>
2018-11-22 11:36:37 -0500

    rustfmt

--- amethyst_utils/src/render.rs
+++ amethyst_utils/src/render.rs
@@ -12,18 +12,27 @@ pub struct MaterialCreator<'a> {
 }
 
 impl<'a> MaterialCreator<'a> {
-
     /// Generate a `Material` from a color.
-    pub fn material_from_color<T: Progress>(&self, color: [f32; 4], progress_counter: T) -> Material {
-        let albedo = self.loader.load_from_data(color.into(), progress_counter, &self.storage);
+    pub fn material_from_color<T: Progress>(
+        &self,
+        color: [f32; 4],
+        progress_counter: T,
+    ) -> Material {
+        let albedo = self
+            .loader
+            .load_from_data(color.into(), progress_counter, &self.storage);
         self.material_from_texture(albedo)
     }
 
     /// Generate a `Material` from a path pointing to a png image.
     pub fn material_from_png<T: Progress>(&self, path: String, progress_counter: T) -> Material {
-        self.material_from_texture(
-            self.loader.load(path, PngFormat, TextureMetadata::srgb(), progress_counter, &self.storage)
-        )
+        self.material_from_texture(self.loader.load(
+            path,
+            PngFormat,
+            TextureMetadata::srgb(),
+            progress_counter,
+            &self.storage,
+        ))
     }
 
     /// Generate a `Material` from a texture handle.
