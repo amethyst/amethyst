0d0a50716e4d0e123ef823cf2b37021e4085e925
Richard Dodd (dodj) <richard.o.dodd@gmail.com>
2018-12-13 19:42:58 +0000

    Make into doc comments

--- amethyst_input/src/bindings.rs
+++ amethyst_input/src/bindings.rs
@@ -18,10 +18,10 @@ where
     AC: Hash + Eq,
 {
     pub(super) axes: HashMap<AX, Axis>,
-    // The inner array here is for button combinations, the other is for different possibilities.
-    //
-    // So for example if you want to quit by either "Esc" or "Ctrl+q" you would have
-    // `[[Esc], [Ctrl, Q]]`.
+    /// The inner array here is for button combinations, the other is for different possibilities.
+    ///
+    /// So for example if you want to quit by either "Esc" or "Ctrl+q" you would have
+    /// `[[Esc], [Ctrl, Q]]`.
     pub(super) actions: HashMap<AC, SmallVec<[SmallVec<[Button; 2]>; 4]>>,
 }
 
