847c9931a21d771e3584865360f23ee620d4abfc
Nolan Darilek <nolan@thewordnerd.info>
2019-01-30 12:29:45 -0600

    Initialize `AudioSystem` as part of `AudioBundle`.
    I think this was an oversight of the initial `DjSystem` removal. The Pong example wasn't even updated to add a separate `AudioSystem`, and it doesn't make sense to me that `AudioBundle` shouldn't do this automatically.

--- amethyst_audio/src/bundle.rs
+++ amethyst_audio/src/bundle.rs
@@ -4,11 +4,11 @@ use amethyst_assets::Processor;
 use amethyst_core::{bundle::SystemBundle, specs::prelude::DispatcherBuilder};
 use amethyst_error::Error;
 
-use crate::source::*;
+use crate::{source::*, systems::AudioSystem};
 
 /// Audio bundle
 ///
-/// This will only add the asset processor for `Source`.
+/// This will only add the audio system and the asset processor for `Source`.
 ///
 /// `DjSystem` must be added separately if you want to use our background music system.
 ///
@@ -16,6 +16,7 @@ pub struct AudioBundle;
 
 impl<'a, 'b> SystemBundle<'a, 'b> for AudioBundle {
     fn build(self, builder: &mut DispatcherBuilder<'a, 'b>) -> Result<(), Error> {
+        builder.add(AudioSystem::new(), "audio_system", &[]);
         builder.add(Processor::<Source>::new(), "source_processor", &[]);
         Ok(())
     }
