edd4579de0dc272d8316908692f860cee46d1c34
Sebastian Waisbrot <seppo0010@gmail.com>
2019-02-08 17:51:20 -0300

    Allow setting a window icon programatically
    Setting a window icon from a file path in the config file is already
    allowed. Now the icon can be set from an image generated
    programatically, for example in the new example a red square is painted.

--- tests/amethyst_test/src/amethyst_application.rs
+++ tests/amethyst_test/src/amethyst_application.rs
@@ -629,6 +629,7 @@ where
             dimensions: Some((SCREEN_WIDTH, SCREEN_HEIGHT)),
             min_dimensions: Some((SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
             max_dimensions: None,
+            loaded_icon: None,
             icon: None,
             vsync: true,
             multisampling: 0, // Must be multiple of 2, use 0 to disable
