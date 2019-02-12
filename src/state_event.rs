821b504da9569f107a324db14d3623b35d500cfe
Joël Lupien (Jojolepro) <jojolepromain@gmail.com>
2019-02-12 12:56:14 -0500

    Initial test for input in StateEvent.

--- src/state_event.rs
+++ src/state_event.rs
@@ -5,16 +5,19 @@ use crate::{
         EventReader,
     },
     derive::EventReader,
+    input::InputEvent,
     renderer::Event,
     ui::UiEvent,
 };
 
 /// The enum holding the different types of event that can be received in a `State` in the handle_event method.
+/// I is the generic type of virtual input events. See `InputEvent<T>` for more information.
 #[derive(Clone, EventReader)]
 #[reader(StateEventReader)]
-pub enum StateEvent {
+pub enum StateEvent<I> {
     /// Events sent by the winit window.
     Window(Event),
     /// Events sent by the ui system.
     Ui(UiEvent),
+    Input(InputEvent::<I>),
 }
