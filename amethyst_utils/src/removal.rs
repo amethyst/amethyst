329ea4129e3661239329441060415b37452f3b6a
jojo <jojolepromain@gmail.com>
2019-03-04 13:08:40 -0500

    added add_removal_to_entity function.

--- amethyst_utils/src/removal.rs
+++ amethyst_utils/src/removal.rs
@@ -60,3 +60,16 @@ pub fn exec_removal<I, D>(
         }
     }
 }
+
+/// Adds a `Removal` component with the specified id to the specified entity.
+/// Usually used with prefabs, when you want to add a `Removal` component at the root of the loaded prefab.
+pub fn add_removal_to_entity<T: PartialEq + Clone + Debug + Send + Sync + 'static>(
+    entity: Entity,
+    id: T,
+    storage: &mut WriteStorage<'_, Removal<T>>,
+) {
+    storage.insert(entity, Removal::new(id)).expect(&format!(
+        "Failed to insert removalid to entity {:?}.",
+        entity
+    ));
+}
