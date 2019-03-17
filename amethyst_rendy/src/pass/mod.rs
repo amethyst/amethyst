4f92348b7d70bc554353e649934405da7221a6ab
Frizi <frizi09@gmail.com>
2019-03-17 16:37:09 +0100

    Chnge shader extensions to match shader type

--- amethyst_rendy/src/pass/mod.rs
+++ amethyst_rendy/src/pass/mod.rs
@@ -8,28 +8,28 @@ use rendy::shader::{ShaderKind, SourceLanguage, StaticShaderInfo};
 
 lazy_static::lazy_static! {
     static ref BASIC_VERTEX: StaticShaderInfo = StaticShaderInfo::new(
-        concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/vertex/basic.glsl"),
+        concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/vertex/basic.vert"),
         ShaderKind::Vertex,
         SourceLanguage::GLSL,
         "main",
     );
 
     static ref FLAT_FRAGMENT: StaticShaderInfo = StaticShaderInfo::new(
-        concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/fragment/flat.glsl"),
+        concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/fragment/flat.frag"),
         ShaderKind::Fragment,
         SourceLanguage::GLSL,
         "main",
     );
 
     static ref PBM_FRAGMENT: StaticShaderInfo = StaticShaderInfo::new(
-        concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/fragment/pbm.glsl"),
+        concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/fragment/pbm.frag"),
         ShaderKind::Fragment,
         SourceLanguage::GLSL,
         "main",
     );
 
     static ref SKINNED_VERTEX: StaticShaderInfo = StaticShaderInfo::new(
-        concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/vertex/skinned.glsl"),
+        concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/vertex/skinned.frag"),
         ShaderKind::Fragment,
         SourceLanguage::GLSL,
         "main",
