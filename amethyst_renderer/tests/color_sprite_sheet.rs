269823f3f273c5f0b9732ff97694346591865d5f
Azriel Hoh <azriel91@gmail.com>
2019-03-21 08:12:26 +1300

    Corrected test name strings.

--- amethyst_renderer/tests/color_sprite_sheet.rs
+++ amethyst_renderer/tests/color_sprite_sheet.rs
@@ -6,7 +6,7 @@ use amethyst_test::AmethystApplication;
 fn solid_returns_sprite_render() -> Result<(), Error> {
     const RED: [f32; 4] = [1., 0.2, 0.1, 1.];
 
-    AmethystApplication::render_base("solid_returns_sprite_render_with_color", false)
+    AmethystApplication::render_base("solid_returns_sprite_render", false)
         .with_setup(|world| {
             let sprite_render = {
                 let color_sprite_gen_data = world.system_data::<ColorSpriteSheetGenData<'_>>();
@@ -39,7 +39,7 @@ fn gradient_returns_sprite_render() -> Result<(), Error> {
     const COLOR_BEGIN: [f32; 4] = [1., 0., 0., 0.5];
     const COLOR_END: [f32; 4] = [0., 1., 0., 1.];
 
-    AmethystApplication::render_base("solid_returns_sprite_render_with_color", false)
+    AmethystApplication::render_base("gradient_returns_sprite_render", false)
         .with_setup(|world| {
             let sprite_render = {
                 let color_sprite_gen_data = world.system_data::<ColorSpriteSheetGenData<'_>>();
