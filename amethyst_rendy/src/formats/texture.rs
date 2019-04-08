025fd1251f7bfb4413ea05d50d4c12184f910535
qthree <qthree3@gmail.com>
2019-04-08 19:27:58 +0700

    Implement TextureGenerator

--- amethyst_rendy/src/formats/texture.rs
+++ amethyst_rendy/src/formats/texture.rs
@@ -5,7 +5,7 @@ use amethyst_assets::{
 use amethyst_core::ecs::{Entity, Read, ReadExpect};
 use amethyst_error::Error;
 use rendy::{
-    hal::Backend,
+    hal::{self, Backend, image::{Filter, Kind, ViewKind, Size}},
     texture::{
         image::{load_from_image, ImageTextureConfig},
         TextureBuilder,
@@ -45,12 +45,57 @@ where
     /// Texture data
     Data(TextureBuilder<'static>),
 
+    // Generate texture
+    Generate(TextureGenerator),
+
     /// Load file with format
     File(String, F, F::Options),
 
     /// Clone handle only
     #[serde(skip)]
     Handle(Handle<Texture<B>>),
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum TextureGenerator {
+    Rgba(f32, f32, f32, f32),
+    RgbaCorners([f32; 16], Filter),
+}
+
+impl TextureGenerator {
+    fn generate(&self) -> (Vec<u8>, Size, Filter) {
+        fn float_to_byte(float: &f32) -> u8 {
+            (float * 255.0).max(0.0).min(255.0) as u8
+        }
+        match self {
+            TextureGenerator::Rgba(red, green, blue, alpha) =>
+            (
+                vec![
+                    float_to_byte(red),
+                    float_to_byte(green),
+                    float_to_byte(blue),
+                    float_to_byte(alpha),
+                ],
+                1,
+                Filter::Nearest,
+            ),
+            TextureGenerator::RgbaCorners(corners, filter) => (
+                corners.iter().map(float_to_byte).collect(),
+                2,
+                *filter
+            ),
+        }
+    }
+    fn data(&self) -> TextureBuilder<'static> {
+        let (data, size, filter) = self.generate();
+        TextureBuilder::new()
+            .with_kind(Kind::D2(size,size,1,1))
+            .with_view_kind(ViewKind::D2)
+            .with_data_width(size)
+            .with_data_height(size)
+            .with_filter(filter)
+            .with_raw_data(data, hal::format::Format::Rgba8Srgb)
+    }
 }
 
 impl<'a, B: Backend, F> PrefabData<'a> for TexturePrefab<B, F>
@@ -76,6 +121,13 @@ where
                     .load_from_data(data.clone(), (), &system_data.1)
             }
 
+            TexturePrefab::Generate(ref generator) => {
+                let data = generator.data();
+                system_data
+                    .0
+                    .load_from_data(data, (), &system_data.1)
+            },
+
             TexturePrefab::File(..) => unreachable!(),
 
             TexturePrefab::Handle(ref handle) => handle.clone(),
@@ -94,6 +146,15 @@ where
                 progress,
                 &system_data.1,
             )),
+
+            TexturePrefab::Generate(ref generator) => {
+                let data = generator.data();
+                Some(system_data.0.load_from_data(
+                    data,
+                    progress,
+                    &system_data.1,
+                ))
+            },
 
             TexturePrefab::File(ref name, ref format, ref options) => Some(system_data.0.load(
                 name.as_ref(),
