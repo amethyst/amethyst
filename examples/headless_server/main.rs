56d62e9c78c0977f80f31964895a9f2766412989
Fletcher Haynes <fletcher@capitalprawn.com>
2019-04-24 01:32:45 -0700

    Added a headless server example

--- examples/headless_server/main.rs
+++ examples/headless_server/main.rs
@@ -1,27 +1,73 @@
-//! A simple headless server
+use amethyst::{
+    core::frame_limiter::FrameRateLimitStrategy,
+    ecs::{Join, System, WriteStorage},
+    network::*,
+    prelude::*,
+    shrev::ReaderId,
+    Result,
+};
 
-use amethyst::prelude::*;
+use log::info;
 
-struct Headless;
+use std::time::Duration;
 
-impl EmptyState for Headless {
-    fn on_start(&mut self, _: StateData<'_, ()>) {
-        let server_addr: SocketAddr = "127.0.0.1:25200".parse().unwrap();
-        println!("Begin!");
-    }
+fn main() -> Result<()> {
+    amethyst::start_logger(Default::default());
 
-    fn on_stop(&mut self, _: StateData<'_, ()>) {
-        println!("End!");
-    }
+    let game_data = GameDataBuilder::default()
+        .with_bundle(NetworkBundle::<()>::new(
+            "127.0.0.1:23455".parse().unwrap(),
+            vec![Box::new(FilterConnected::<()>::new())],
+        ))?
+        .with(SpamReceiveSystem::new(), "rcv", &[]);
+    let mut game = Application::build("./", State1)?
+        .with_frame_limit(
+            FrameRateLimitStrategy::SleepAndYield(Duration::from_millis(2)),
+            1,
+        )
+        .build(game_data)?;
+    game.run();
+    Ok(())
+}
 
-    fn update(&mut self, _: StateData<'_, ()>) -> EmptyTrans {
-        println!("Hello from Headless Amethyst!");
-        Trans::Quit
+/// Default empty state
+pub struct State1;
+impl SimpleState for State1 {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        data.world
+            .create_entity()
+            .with(NetConnection::<()>::new("127.0.0.1:3457".parse().unwrap()))
+            .build();
     }
 }
 
-fn main() {
-    amethyst::start_logger(Default::default());
-    let mut game = Application::new("./", Headless, ()).expect("Fatal error");
-    game.run();
+/// A simple system that receives a ton of network events.
+struct SpamReceiveSystem {
+    pub reader: Option<ReaderId<NetEvent<()>>>,
+}
+
+impl SpamReceiveSystem {
+    pub fn new() -> Self {
+        SpamReceiveSystem { reader: None }
+    }
+}
+
+impl<'a> System<'a> for SpamReceiveSystem {
+    type SystemData = (WriteStorage<'a, NetConnection<()>>,);
+    fn run(&mut self, (mut connections,): Self::SystemData) {
+        let mut count = 0;
+        for (conn,) in (&mut connections,).join() {
+            if self.reader.is_none() {
+                self.reader = Some(conn.receive_buffer.register_reader());
+            }
+            for ev in conn.receive_buffer.read(self.reader.as_mut().unwrap()) {
+                count += 1;
+                match ev {
+                    &NetEvent::TextMessage { ref msg } => info!("{}", msg),
+                    _ => {}
+                }
+            }
+        }
+        info!("Received {} messages this frame", count);
+    }
 }
