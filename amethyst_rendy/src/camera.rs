5f74fefd70b8c852637144ba8d057ec200bde64b
Rudi Floren <rudi.floren@gmail.com>
2019-05-15 21:41:48 +0200

    Add some ideas for tests.

--- amethyst_rendy/src/camera.rs
+++ amethyst_rendy/src/camera.rs
@@ -25,15 +25,15 @@ pub enum Projection {
 impl Projection {
     /// Creates an orthographic projection with the given left, right, bottom, and
     /// top plane distances.
-    /// Projection matrix is right-handed and has a depth range of 0 to 1
+    /// The projection matrix is right-handed and has a depth range of 0 to 1
     pub fn orthographic(left: f32, right: f32, bottom: f32, top: f32, z_near: f32, z_far: f32,) -> Projection {
         let mut proj = Matrix4::<f32>::identity();
 
         proj[(0, 0)] = 2.0 / (right - left);
-        proj[(0, 3)] = - (right + left) / (right - left);
         proj[(1, 1)] = 2.0 / (top - bottom);
-        proj[(1, 3)] = - (top + bottom) / (top - bottom);
         proj[(2, 2)] = - 1.0 / (z_far - z_near);
+        proj[(0, 3)] = - (right + left) / (right - left);
+        proj[(1, 3)] = - (top + bottom) / (top - bottom);
         proj[(2, 3)] = - z_near / (z_far  - z_near);
 
         // Important: nalgebra's methods on Orthographic3 are not safe for use with RH matrices
@@ -42,7 +42,7 @@ impl Projection {
 
     /// Creates a perspective projection with the given aspect ratio and
     /// field-of-view. `fov` is specified in radians.
-    /// Projection matrix is right-handed and has a depth range of 0 to 1
+    /// The projection matrix is right-handed and has a depth range of 0 to 1
     pub fn perspective(aspect: f32, fov: f32, z_near: f32, z_far: f32) -> Projection {
         let mut proj = Matrix4::<f32>::identity();
         
@@ -50,8 +50,8 @@ impl Projection {
 
         proj[(0, 0)] = 1.0 / (aspect * tan_half_fovy);
         proj[(1, 1)] = - 1.0 / tan_half_fovy;
-
         proj[(2, 2)] = z_far / (z_near - z_far);
+
         proj[(2, 3)] = - (z_near * z_far) / (z_far - z_near);
         proj[(3, 2)] = - 1.0;
         proj[(3, 3)] = 0.0;
@@ -275,34 +275,83 @@ mod serde_persp {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use amethyst_core::math;
+
+    static ASPECT_RATIO : f32 = 16.0/9.0;
+
+
     #[test]
     fn standard_2d() {
         let cam = Camera::standard_2d(1280.0, 720.0);
-
+        
     }
 
     #[test]
     fn standard_3d() {
-        let cam = Camera::standard_3d(1280.0, 720.0)
+        let cam = Camera::standard_3d(1280.0, 720.0);
+
     }
 
     #[test]
     fn orthographic_orientation() {
-        let cam : Camera = Projection::orthographic(-1.0, 1.0, 1.0, -1.0, 0.1, 2000.0).into()
+        let cam : Camera = Projection::orthographic(-1.0, 1.0, -1.0, 1.0, 0.1, 100.0).into();
+        let vec_in1 = math::Vector4::<f32>::new(0.0, 0.0, -1.0, 1.0);
+        let vec_out1 = cam.proj * vec_in1;
+        let vec_out1_ref = math::Vector4::<f32>::new(0.0, 0.0, -1.0 / (100.0 - 0.1) * vec_in1[2] - 0.1 / (100.0 - 0.1), 1.0);
+        assert_eq!(vec_out1_ref, vec_out1);
+
+        let vec_in2 = math::Vector4::<f32>::new(0.5, 0.5, -99.0, 1.0);
+        let vec_out2 = cam.proj * vec_in2;
+        let z_c = -1.0 / (100.0 - 0.1) * vec_in2[2] - 0.1 / (100.0 - 0.1);
+
+        let vec_out2_ref = math::Vector4::<f32>::new(0.5, 0.5, z_c, 1.0);
+        assert_eq!(vec_out2_ref, vec_out2);
+
     }
 
     #[test]
     fn orthographic_depth() {
-        let cam : Camera = Projection::orthographic(-1.0, 1.0, 1.0, -1.0, 0.1, 2000.0).into()
+        let cam : Camera = Projection::orthographic(-10.0, 10.0, -10.0, 10.0, 0.1, 100.0).into();
+        let vec_in1 = math::Vector4::<f32>::new(0.0, -5.0, -0.1, 1.0);
+        let vec_out1 = cam.proj * vec_in1;
+        // let vec_out1_ref = math::Vector4::<f32>::new(0.0, -0.5, 0.002002002, 1.0);
+
+        // let vec_in2 = math::Vector4::<f32>::new(0.0, -2.0, -50.1, 1.0);
+        // let vec_out2 = cam.proj * vec_in2;
+        // let vec_out2_ref = math::Vector4::<f32>::new(0.0, 0.4, 50.1, 1.0);
+
+        // let vec_in3 = math::Vector4::<f32>::new(0.0, 2.0, -99.0, 1.0);
+        // let vec_out3 = cam.proj * vec_in3;
+        // let vec_out3_ref = math::Vector4::<f32>::new(0.0, -0.4, 99.0, 1.0);
+
+        // let vec_in4 = math::Vector4::<f32>::new(0.0, 5.0, -100.1, 1.0);
+        // let vec_out4 = cam.proj * vec_in4;
+        // let vec_out4_ref = math::Vector4::<f32>::new(0.0, 0.4, 100.1, 1.0);
+
+        // assert_eq!(vec_out1_ref, vec_out1);
+        // assert_eq!(vec_out2_ref, vec_out2);
+        // assert_eq!(vec_out3_ref, vec_out3);
+        // assert_eq!(vec_out4_ref, vec_out4);
     }
 
     #[test]
     fn perspective_orientation() {
-        let cam : Camera = Projection:perspective(16.0/9.0, std::f32::consts::FRAC_PI_3, 0.1, 2000.0).into();
+        let cam : Camera = Projection::perspective(ASPECT_RATIO, std::f32::consts::FRAC_PI_3, 0.1, 100.0).into();
+        let vec_in1 = math::Vector4::<f32>::new(0.0, 0.0, -0.11, 1.0);
+        let vec_out1 = cam.proj * vec_in1;
+        assert!(vec_out1[2] >= 0.0);
+        assert!(vec_out1[2] <= 1.0);
+
+        let vec_in2 = math::Vector4::<f32>::new(0.0, 1.0, -99.0, 1.0);
+        let vec_out2 = cam.proj * vec_in2;
+        dbg!(vec_out2);
+        assert!(vec_out2[2] >= 0.0);
+        assert!(vec_out2[2] <= 1.0);
     }
 
     #[test]
     fn perspective_depth() {
-        let cam : Camera = Projection:perspective(16.0/9.0, std::f32::consts::FRAC_PI_3, 0.1, 2000.0).into();
+        let cam : Camera = Projection::perspective(ASPECT_RATIO, std::f32::consts::FRAC_PI_3, 0.1 as f32, 2000.0).into();
     }
 }
