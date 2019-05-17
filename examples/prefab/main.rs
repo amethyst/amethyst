c2638ebf147f822ee730ffc6ba304daf9ebc08e1
Frizi <frizi09@gmail.com>
2019-05-17 19:22:08 +0200

    Remove unused backend generic from animation example

--- examples/prefab/main.rs
+++ examples/prefab/main.rs
@@ -4,8 +4,21 @@ use amethyst::{
     assets::{PrefabLoader, PrefabLoaderSystem, RonFormat},
     core::TransformBundle,
     prelude::*,
-    renderer::{DrawShaded, PosNormTex},
+    renderer::{
+        rendy::{
+            factory::Factory,
+            graph::{
+                render::{RenderGroupDesc, SubpassBuilder},
+                GraphBuilder,
+            },
+            hal::format::Format,
+            mesh::{Normal, Position, TexCoord},
+        },
+        types::DefaultBackend,
+        GraphCreator, RenderingSystem,
+    },
     utils::{application_root_dir, scene::BasicScenePrefab},
+    window::{ScreenDimensions, Window, WindowBundle},
     Error,
 };
 
@@ -34,11 +47,82 @@ fn main() -> Result<(), Error> {
     let display_config_path = app_root.join("examples/prefab/resources/display_config.ron");
 
     let game_data = GameDataBuilder::default()
+        .with_bundle(WindowBundle::from_config_path(display_config_path))?
         .with(PrefabLoaderSystem::<MyPrefabData>::default(), "", &[])
         .with_bundle(TransformBundle::new())?
-        .with_basic_renderer(display_config_path, DrawShaded::<PosNormTex>::new(), false)?;
-
+        .with_thread_local(RenderingSystem::<DefaultBackend, _>::new(
+            ExampleGraph::default(),
+        ));
     let mut game = Application::new(resources_directory, AssetsExample, game_data)?;
     game.run();
     Ok(())
+}
+
+#[derive(Default)]
+struct ExampleGraph {
+    last_dimensions: Option<ScreenDimensions>,
+    surface_format: Option<Format>,
+    dirty: bool,
+}
+
+impl GraphCreator<DefaultBackend> for ExampleGraph {
+    fn rebuild(&mut self, res: &Resources) -> bool {
+        // Rebuild when dimensions change, but wait until at least two frames have the same.
+        let new_dimensions = res.try_fetch::<ScreenDimensions>();
+        use std::ops::Deref;
+        if self.last_dimensions.as_ref() != new_dimensions.as_ref().map(|d| d.deref()) {
+            self.dirty = true;
+            self.last_dimensions = new_dimensions.map(|d| d.clone());
+            return false;
+        }
+        return self.dirty;
+    }
+
+    fn builder(
+        &mut self,
+        factory: &mut Factory<DefaultBackend>,
+        res: &Resources,
+    ) -> GraphBuilder<DefaultBackend, Resources> {
+        use amethyst::renderer::rendy::{
+            graph::present::PresentNode,
+            hal::command::{ClearDepthStencil, ClearValue},
+        };
+
+        self.dirty = false;
+
+        let window = <ReadExpect<'_, std::sync::Arc<Window>>>::fetch(res);
+        let surface = factory.create_surface(window.clone());
+        // cache surface format to speed things up
+        let surface_format = *self
+            .surface_format
+            .get_or_insert_with(|| factory.get_surface_format(&surface));
+
+        let mut graph_builder = GraphBuilder::new();
+        let color = graph_builder.create_image(
+            surface.kind(),
+            1,
+            surface_format,
+            Some(ClearValue::Color([0.34, 0.36, 0.52, 1.0].into())),
+        );
+
+        let depth = graph_builder.create_image(
+            surface.kind(),
+            1,
+            Format::D32Float,
+            Some(ClearValue::DepthStencil(ClearDepthStencil(1.0, 0))),
+        );
+
+        let ui = graph_builder.add_node(
+            SubpassBuilder::new()
+                .with_group(DrawUiDesc::default().builder())
+                .with_color(color)
+                .with_depth_stencil(depth)
+                .into_pass(),
+        );
+
+        let _present = graph_builder
+            .add_node(PresentNode::builder(factory, surface, color).with_dependency(ui));
+
+        graph_builder
+    }
 }
