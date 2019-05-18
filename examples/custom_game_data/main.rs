d136450db8be58534073403d7ce3cd55c3a69a7f
Rudi Floren <rudi.floren@gmail.com>
2019-05-18 04:55:49 +0200

    Remove debug_lines

--- examples/custom_game_data/main.rs
+++ examples/custom_game_data/main.rs
@@ -13,7 +13,6 @@ use amethyst::{
     prelude::*,
     renderer::{
         Mesh,
-        debug_drawing::{DebugLines, DebugLinesComponent, DebugLinesParams},
         palette::{Srgb, Srgba},
         rendy::{
             factory::Factory,
@@ -24,7 +23,7 @@ use amethyst::{
             hal::format::Format,
             mesh::{Normal, Position, TexCoord},
         },
-        pass::DrawShadedDesc, pass::DrawDebugLinesDesc,
+        pass::DrawShadedDesc,
         types::DefaultBackend,
         GraphCreator, RenderingSystem,
     },
@@ -164,82 +163,6 @@ impl<'a, 'b> State<CustomGameData<'a, 'b>, StateEvent> for Paused {
 impl<'a, 'b> State<CustomGameData<'a, 'b>, StateEvent> for Main {
     fn on_start(&mut self, data: StateData<'_, CustomGameData<'_, '_>>) {
         data.world.create_entity().with(self.scene.clone()).build();
-
-
-        // Setup debug lines as a component and add lines to render axis&grid
-        let mut debug_lines_component = DebugLinesComponent::with_capacity(100);
-        debug_lines_component.add_direction(
-            [0.0, 0.0001, 0.0].into(),
-            [0.2, 0.0, 0.0].into(),
-            Srgba::new(1.0, 0.0, 0.23, 1.0),
-        );
-        debug_lines_component.add_direction(
-            [0.0, 0.0, 0.0].into(),
-            [0.0, 0.2, 0.0].into(),
-            Srgba::new(0.5, 0.85, 0.1, 1.0),
-        );
-        debug_lines_component.add_direction(
-            [0.0, 0.0001, 0.0].into(),
-            [0.0, 0.0, 0.2].into(),
-            Srgba::new(0.2, 0.75, 0.93, 1.0),
-        );
-
-        let width: u32 = 50;
-        let depth: u32 = 50;
-        let main_color = Srgba::new(0.4, 0.4, 0.4, 1.0);
-
-        // Grid lines in X-axis
-        for x in 0..=width {
-            let (x, width, depth) = (x as f32, width as f32, depth as f32);
-
-            let position = Point3::new(x - width / 2.0, 0.0, -depth / 2.0);
-            let direction = Vector3::new(0.0, 0.0, depth);
-
-            debug_lines_component.add_direction(position, direction, main_color);
-
-            // Sub-grid lines
-            if x != width {
-                for sub_x in 1..10 {
-                    let sub_offset = Vector3::new((1.0 / 10.0) * sub_x as f32, -0.001, 0.0);
-
-                    debug_lines_component.add_direction(
-                        position + sub_offset,
-                        direction,
-                        Srgba::new(0.1, 0.1, 0.1, 0.1),
-                    );
-                }
-            }
-        }
-
-        // Grid lines in Z-axis
-        for z in 0..=depth {
-            let (z, width, depth) = (z as f32, width as f32, depth as f32);
-
-            let position = Point3::new(-width / 2.0, 0.0, z - depth / 2.0);
-            let direction = Vector3::new(width, 0.0, 0.0);
-
-            debug_lines_component.add_direction(position, direction, main_color);
-
-            // Sub-grid lines
-            if z != depth {
-                for sub_z in 1..10 {
-                    let sub_offset = Vector3::new(0.0, -0.001, (1.0 / 10.0) * sub_z as f32);
-
-                    debug_lines_component.add_direction(
-                        position + sub_offset,
-                        direction,
-                        Srgba::new(0.1, 0.1, 0.1, 0.0),
-                    );
-                }
-            }
-        }
-        data.world.register::<DebugLinesComponent>();
-        data.world
-            .create_entity()
-            .with(debug_lines_component)
-            .build();
-
-
     }
 
     fn handle_event(
@@ -371,7 +294,6 @@ impl GraphCreator<DefaultBackend> for ExampleGraph {
         );
         let opaque = graph_builder.add_node(
             SubpassBuilder::new()
-                .with_group(DrawDebugLinesDesc::default().builder())
                 .with_group(DrawShadedDesc::default().builder())
                 .with_color(color)
                 .with_depth_stencil(depth)
