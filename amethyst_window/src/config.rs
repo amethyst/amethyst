1ff1eeae76113baefc28e36500a1efafcacb366b
Thomas Schaller <torkleyy@gmail.com>
2019-05-18 11:10:17 +0200

    Add back advanced window attributes to DisplayConfig

--- amethyst_window/src/config.rs
+++ amethyst_window/src/config.rs
@@ -23,6 +23,26 @@ pub struct DisplayConfig {
     pub max_dimensions: Option<(u32, u32)>,
     #[serde(default = "default_visibility")]
     pub visibility: bool,
+    /// Whether the window should always be on top of other windows.
+    #[serde(default)]
+    pub always_on_top: bool,
+    /// Whether the window should have borders and bars.
+    #[serde(default = "default_decorations")]
+    pub decorations: bool,
+    /// Whether the window should be maximized upon creation.
+    #[serde(default)]
+    pub maximized: bool,
+    /// Enable multitouch on iOS.
+    #[serde(default)]
+    pub multitouch: bool,
+    /// Whether the window is resizable or not.
+    #[serde(default = "default_resizable")]
+    pub resizable: bool,
+    /// Whether the the window should be transparent. If this is true, writing
+    /// colors with alpha values different than 1.0 will produce a transparent
+    /// window.
+    #[serde(default)]
+    pub transparent: bool,
 }
 
 impl Default for DisplayConfig {
@@ -34,6 +54,12 @@ impl Default for DisplayConfig {
             min_dimensions: None,
             max_dimensions: None,
             visibility: default_visibility(),
+            always_on_top: false,
+            decorations: default_decorations(),
+            maximized: false,
+            multitouch: false,
+            resizable: default_resizable(),
+            transparent: false
         }
     }
 }
@@ -41,7 +67,16 @@ impl Default for DisplayConfig {
 fn default_title() -> String {
     "Amethyst game".to_string()
 }
+
+fn default_decorations() -> bool {
+    true
+}
+
 fn default_visibility() -> bool {
+    true
+}
+
+fn default_resizable() -> bool {
     true
 }
 
@@ -55,9 +90,15 @@ impl DisplayConfig {
             max_dimensions: self.max_dimensions.map(Into::into),
             min_dimensions: self.min_dimensions.map(Into::into),
             title: self.title,
+            maximized: self.maximized,
             visible: self.visibility,
+            transparent: self.transparent,
+            decorations: self.decorations,
+            always_on_top: self.always_on_top,
+            window_icon: None,
             fullscreen: self.fullscreen.map(|ident| ident.monitor_id(monitors)),
-            ..Default::default()
+            resizable: self.resizable,
+            multitouch: self.multitouch
         };
 
         let mut builder = WindowBuilder::new();
