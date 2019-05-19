f93ddab69b9064ee8d4bf6554e532adb5ffab1b9
Frizi <frizi09@gmail.com>
2019-05-19 18:45:16 +0200

    Remove appendix_a and port spotlights example to rendy

--- examples/spotlights/main.rs
+++ examples/spotlights/main.rs
@@ -1,19 +1,34 @@
 use amethyst::{
     assets::{PrefabLoader, PrefabLoaderSystem, RonFormat},
     core::transform::TransformBundle,
+    ecs::{ReadExpect, Resources, SystemData},
     prelude::*,
-    renderer::{DrawPbm, PosNormTangTex},
+    renderer::{
+        pass::DrawPbrDesc,
+        rendy::{
+            factory::Factory,
+            graph::{
+                render::{RenderGroupDesc, SubpassBuilder},
+                GraphBuilder,
+            },
+            hal::format::Format,
+            mesh::{Normal, Position, Tangent, TexCoord},
+        },
+        types::DefaultBackend,
+        GraphCreator, RenderingSystem,
+    },
     utils::{application_root_dir, scene::BasicScenePrefab},
+    window::{ScreenDimensions, Window, WindowBundle},
 };
 
-type MyPrefabData = BasicScenePrefab<Vec<PosNormTangTex>>;
+type MyPrefabData = BasicScenePrefab<(Vec<Position>, Vec<Normal>, Vec<Tangent>, Vec<TexCoord>)>;
 
 struct Example;
 
 impl SimpleState for Example {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let handle = data.world.exec(|loader: PrefabLoader<'_, MyPrefabData>| {
-            loader.load("prefab/spotlights_scene.ron", RonFormat, (), ())
+            loader.load("prefab/spotlights_scene.ron", RonFormat, ())
         });
         data.world.create_entity().with(handle).build();
     }
@@ -23,16 +38,86 @@ fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
 
     let app_root = application_root_dir()?;
-
     let display_config_path = app_root.join("examples/spotlights/resources/display_config.ron");
-
     let resources = app_root.join("examples/assets/");
 
     let game_data = GameDataBuilder::default()
+        .with_bundle(WindowBundle::from_config_path(display_config_path))?
         .with(PrefabLoaderSystem::<MyPrefabData>::default(), "", &[])
         .with_bundle(TransformBundle::new())?
-        .with_basic_renderer(display_config_path, DrawPbm::<PosNormTangTex>::new(), false)?;
+        .with_thread_local(RenderingSystem::<DefaultBackend, _>::new(
+            ExampleGraph::default(),
+        ));
+
     let mut game = Application::new(resources, Example, game_data)?;
     game.run();
     Ok(())
+}
+
+#[derive(Default)]
+struct ExampleGraph {
+    last_dimensions: Option<ScreenDimensions>,
+    surface_format: Option<Format>,
+    dirty: bool,
+}
+
+impl GraphCreator<DefaultBackend> for ExampleGraph {
+    fn rebuild(&mut self, res: &Resources) -> bool {
+        // Rebuild when dimensions change, but wait until at least two frames have the same.
+        let new_dimensions = res.try_fetch::<ScreenDimensions>();
+        use std::ops::Deref;
+        if self.last_dimensions.as_ref() != new_dimensions.as_ref().map(|d| d.deref()) {
+            self.dirty = true;
+            self.last_dimensions = new_dimensions.map(|d| d.clone());
+            return false;
+        }
+        return self.dirty;
+    }
+
+    fn builder(
+        &mut self,
+        factory: &mut Factory<DefaultBackend>,
+        res: &Resources,
+    ) -> GraphBuilder<DefaultBackend, Resources> {
+        use amethyst::renderer::rendy::{
+            graph::present::PresentNode,
+            hal::command::{ClearDepthStencil, ClearValue},
+        };
+
+        self.dirty = false;
+        let window = <ReadExpect<'_, std::sync::Arc<Window>>>::fetch(res);
+        let surface = factory.create_surface(window.clone());
+        // cache surface format to speed things up
+        let surface_format = *self
+            .surface_format
+            .get_or_insert_with(|| factory.get_surface_format(&surface));
+
+        let mut graph_builder = GraphBuilder::new();
+        let color = graph_builder.create_image(
+            surface.kind(),
+            1,
+            surface_format,
+            Some(ClearValue::Color([0.34, 0.36, 0.52, 1.0].into())),
+        );
+
+        let depth = graph_builder.create_image(
+            surface.kind(),
+            1,
+            Format::D32Sfloat,
+            Some(ClearValue::DepthStencil(ClearDepthStencil(1.0, 0))),
+        );
+
+        let opaque = graph_builder.add_node(
+            SubpassBuilder::new()
+                .with_group(DrawPbrDesc::skinned().builder())
+                .with_color(color)
+                .with_depth_stencil(depth)
+                .into_pass(),
+        );
+
+        let _present = graph_builder
+            .add_node(PresentNode::builder(factory, surface, color).with_dependency(opaque));
+
+        graph_builder
+    }
 }
