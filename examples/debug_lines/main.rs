1cd4a6ebd4839e28098aee973593dd44f3d3caa8
Frizi <frizi09@gmail.com>
2019-05-20 23:58:46 +0200

    Update to latest rendy

--- examples/debug_lines/main.rs
+++ examples/debug_lines/main.rs
@@ -25,6 +25,7 @@ use amethyst::{
             hal::{
                 command::{ClearDepthStencil, ClearValue},
                 format::Format,
+                image,
             },
         },
         types::DefaultBackend,
@@ -210,7 +211,7 @@ fn main() -> amethyst::Result<()> {
 
 #[derive(Default)]
 struct ExampleGraph {
-    last_dimensions: Option<ScreenDimensions>,
+    dimensions: Option<ScreenDimensions>,
     surface_format: Option<Format>,
     dirty: bool,
 }
@@ -220,9 +221,9 @@ impl<B: Backend> GraphCreator<B> for ExampleGraph {
         // Rebuild when dimensions change, but wait until at least two frames have the same.
         let new_dimensions = res.try_fetch::<ScreenDimensions>();
         use std::ops::Deref;
-        if self.last_dimensions.as_ref() != new_dimensions.as_ref().map(|d| d.deref()) {
+        if self.dimensions.as_ref() != new_dimensions.as_ref().map(|d| d.deref()) {
             self.dirty = true;
-            self.last_dimensions = new_dimensions.map(|d| d.clone());
+            self.dimensions = new_dimensions.map(|d| d.clone());
             return false;
         }
         return self.dirty;
@@ -233,22 +234,25 @@ impl<B: Backend> GraphCreator<B> for ExampleGraph {
 
         let window = <ReadExpect<'_, std::sync::Arc<Window>>>::fetch(res);
 
-        let surface = factory.create_surface(window.clone());
+        let surface = factory.create_surface(&window);
         // cache surface format to speed things up
         let surface_format = *self
             .surface_format
             .get_or_insert_with(|| factory.get_surface_format(&surface));
+        let dimensions = self.dimensions.as_ref().unwrap();
+        let window_kind =
+            image::Kind::D2(dimensions.width() as u32, dimensions.height() as u32, 1, 1);
 
         let mut graph_builder = GraphBuilder::new();
         let color = graph_builder.create_image(
-            surface.kind(),
+            window_kind,
             1,
             surface_format,
             Some(ClearValue::Color([0.34, 0.36, 0.52, 1.0].into())),
         );
 
         let depth = graph_builder.create_image(
-            surface.kind(),
+            window_kind,
             1,
             Format::D32Sfloat,
             Some(ClearValue::DepthStencil(ClearDepthStencil(1.0, 0))),
