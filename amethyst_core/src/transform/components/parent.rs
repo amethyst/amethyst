bfd9401d67bb43776d648910d1016d37f05d64c7
mrhatman <lucasince@gmail.com>
2019-06-15 23:56:43 -0500

    Added Transform Docs
    Added some lines clarifying what the purpose of the parent component to the docs.

--- amethyst_core/src/transform/components/parent.rs
+++ amethyst_core/src/transform/components/parent.rs
@@ -9,6 +9,16 @@ pub type ParentHierarchy = Hierarchy<Parent>;
 /// Component for defining a parent entity.
 ///
 /// The entity with this component *has* a parent, rather than *is* a parent.
+///
+/// If the parent enitity has a transform, then all of that transform 
+/// (scale, then rotation, then translation) will be applied the object
+/// before any of the child's tranform is applied. For example, if a 
+/// parent rotates 45 degrees then child translations will be as if 
+/// the axes are rotated 45 degrees.
+///
+/// If the parent entity has is own parent (and so on) then the transforms 
+/// will all be applied in order from the oldest ancestor to the child.
+
 #[derive(Debug, Clone, Eq, Ord, PartialEq, PartialOrd, new)]
 pub struct Parent {
     /// The parent entity
