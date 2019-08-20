71a101d8ae2a7fcc1b28488adf82796eac51cc86
mrhatman <lucasince@gmail.com>
2019-08-19 22:08:42 -0500

    Update to reuse args
    Updating the rendering code to improve variable reuse and understanding.

--- amethyst_ui/src/pass.rs
+++ amethyst_ui/src/pass.rs
@@ -633,60 +633,88 @@ fn render_image<B: Backend>(
                         (*y+*height-*bottom_dist)as f32/texture_dimensions[1]as f32,
                         (*y+*height)as f32/texture_dimensions[1]as f32];
 
+                let x_dimensions =
+                [
+                    *left_dist as f32,
+                    transform.pixel_width -( *right_dist+*left_dist)as f32,
+                    *right_dist as f32
+                ];
+
+                let y_dimensions =
+                [
+                    *top_dist as f32,
+                    transform.pixel_height -( *top_dist+*right_dist)as f32,
+                    *bottom_dist as f32
+                ];
+
+                let x_coords =
+                [
+                    transform.pixel_x()-((transform.pixel_width - *left_dist as f32) /2.0),
+                    transform.pixel_x()+((*left_dist - *right_dist) as f32/2.0),
+                    transform.pixel_x()+((transform.pixel_width - *right_dist as f32)/2.0)
+                ];
+                let y_coords =
+                [
+                    transform.pixel_y()+((transform.pixel_height - *top_dist as f32)/2.0),
+                    transform.pixel_y()+((*bottom_dist- *top_dist) as f32)/2.0,
+                    transform.pixel_y()-((transform.pixel_height - *bottom_dist as f32)/2.0)
+                ];
+
+
 
                 let mut top_left_args = args.clone();
                 top_left_args.tex_coord_bounds = [x_slices[0],y_slices[0],x_slices[1],y_slices[1]].into();
-                top_left_args.dimensions =  [*left_dist as f32,*top_dist as f32].into();
-                top_left_args.coords =  [transform.pixel_x()-((transform.pixel_width - *left_dist as f32) /2.0),transform.pixel_y()+((transform.pixel_height - *top_dist as f32)/2.0)].into();
+                top_left_args.dimensions =  [x_dimensions[0],y_dimensions[0]].into();
+                top_left_args.coords =  [x_coords[0],y_coords[0]].into();
                 batches.insert(tex_id, Some(top_left_args));
 
                 let mut top_middle_args = args.clone();
                 top_middle_args.tex_coord_bounds = [x_slices[1],y_slices[0],x_slices[2],y_slices[1]].into();
-                top_middle_args.dimensions =  [transform.pixel_width -( *right_dist+*left_dist)as f32,*top_dist as f32].into();
-                top_middle_args.coords =  [transform.pixel_x()+((*left_dist - *right_dist) as f32/2.0),transform.pixel_y()+((transform.pixel_height - *top_dist as f32)/2.0)].into();
+                top_middle_args.dimensions =  [x_dimensions[1],y_dimensions[0]].into();
+                top_middle_args.coords =  [x_coords[1],y_coords[0]].into();
                 batches.insert(tex_id, Some(top_middle_args));
 
                 let mut top_right_args = args.clone();
                 top_right_args.tex_coord_bounds = [x_slices[2],y_slices[0],x_slices[3],y_slices[1]].into();
-                top_right_args.dimensions =  [*right_dist as f32,*top_dist as f32].into();
-                top_right_args.coords =  [transform.pixel_x()+((transform.pixel_width - *right_dist as f32)/2.0),transform.pixel_y()+((transform.pixel_height - *top_dist as f32)/2.0)].into();
+                top_right_args.dimensions =  [x_dimensions[2],y_dimensions[0]].into();
+                top_right_args.coords =  [x_coords[2],y_coords[0]].into();
                 batches.insert(tex_id, Some(top_right_args));
 
                 let mut middle_left_args = args.clone();
                 middle_left_args.tex_coord_bounds = [x_slices[0],y_slices[1],x_slices[1],y_slices[2]].into();
-                middle_left_args.dimensions =  [*left_dist as f32,transform.pixel_height -( *top_dist+*right_dist)as f32].into();
-                middle_left_args.coords =  [transform.pixel_x()-((transform.pixel_width - *left_dist as f32) /2.0),transform.pixel_y()+((*bottom_dist- *top_dist) as f32)/2.0].into();
+                middle_left_args.dimensions =  [x_dimensions[0],y_dimensions[1]].into();
+                middle_left_args.coords =  [x_coords[0],y_coords[1]].into();
                 batches.insert(tex_id, Some(middle_left_args));
 
                 let mut middle_middle_args = args.clone();
                 middle_middle_args.tex_coord_bounds = [x_slices[1],y_slices[1],x_slices[2],y_slices[2]].into();
-                middle_middle_args.dimensions =  [transform.pixel_width -( *right_dist+*left_dist)as f32,transform.pixel_height -( *top_dist+*right_dist)as f32].into();
-                middle_middle_args.coords =  [transform.pixel_x()+((*left_dist - *right_dist) as f32/2.0),transform.pixel_y()+((*bottom_dist- *top_dist) as f32)/2.0].into();
+                middle_middle_args.dimensions =  [x_dimensions[1],y_dimensions[1]].into();
+                middle_middle_args.coords =  [x_coords[1],y_coords[1]].into();
                 batches.insert(tex_id, Some(middle_middle_args));
 
                 let mut middle_right_args = args.clone();
                 middle_middle_args.tex_coord_bounds = [x_slices[2],y_slices[1],x_slices[3],y_slices[2]].into();
-                middle_middle_args.dimensions =  [*right_dist as f32,transform.pixel_height -( *top_dist+*right_dist)as f32].into();
-                middle_middle_args.coords =  [transform.pixel_x()+((transform.pixel_width - *right_dist as f32)/2.0),transform.pixel_y()+((*bottom_dist- *top_dist) as f32)/2.0].into();
+                middle_middle_args.dimensions =  [x_dimensions[2],y_dimensions[1]].into();
+                middle_middle_args.coords =  [x_coords[2],y_coords[1]].into();
                 batches.insert(tex_id, Some(middle_middle_args));
 
 
                 let mut bottom_left_args = args.clone();
                 bottom_left_args.tex_coord_bounds = [x_slices[0],y_slices[2],x_slices[1],y_slices[3]].into();
-                bottom_left_args.dimensions =  [*left_dist as f32,*bottom_dist as f32].into();
-                bottom_left_args.coords =  [transform.pixel_x()-((transform.pixel_width - *left_dist as f32) /2.0),transform.pixel_y()-((transform.pixel_height - *bottom_dist as f32)/2.0)].into();
+                bottom_left_args.dimensions =  [x_dimensions[0],y_dimensions[2]].into();
+                bottom_left_args.coords =  [x_coords[0],y_coords[2]].into();
                 batches.insert(tex_id, Some(bottom_left_args));
 
                 let mut bottom_middle_args = args.clone();
                 bottom_middle_args.tex_coord_bounds = [x_slices[1],y_slices[2],x_slices[2],y_slices[3]].into();
-                bottom_middle_args.dimensions =  [transform.pixel_width -( *right_dist+*left_dist)as f32,*bottom_dist as f32].into();
-                bottom_middle_args.coords =  [transform.pixel_x()+((*left_dist - *right_dist) as f32/2.0),transform.pixel_y()-((transform.pixel_height - *bottom_dist as f32)/2.0)].into();
+                bottom_middle_args.dimensions =  [x_dimensions[1],y_dimensions[2]].into();
+                bottom_middle_args.coords =  [x_coords[1],y_coords[2]].into();
                 batches.insert(tex_id, Some(bottom_middle_args));
 
                 let mut bottom_right_args = args.clone();
                 bottom_right_args.tex_coord_bounds = [x_slices[2],y_slices[2],x_slices[3],y_slices[3]].into();
-                bottom_right_args.dimensions =  [*right_dist as f32,*bottom_dist as f32].into();
-                bottom_right_args.coords =  [transform.pixel_x()+((transform.pixel_width - *right_dist as f32) /2.0),transform.pixel_y()-((transform.pixel_height- *bottom_dist as f32)/2.0)].into();
+                bottom_right_args.dimensions =  [x_dimensions[2],y_dimensions[2]].into();
+                bottom_right_args.coords =  [x_coords[2],y_coords[2]].into();
                 batches.insert(tex_id, Some(bottom_right_args));
 
 
