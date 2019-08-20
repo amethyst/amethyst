fcd1750cdcbefc3305806c5a21d93115ea87a290
Karl Bergström <karl.anton.bergstrom@gmail.com>
2019-08-20 23:52:19 +0200

    Fix HandleRef clone

--- amethyst_assets/src/handle_new.rs
+++ amethyst_assets/src/handle_new.rs
@@ -68,11 +68,10 @@ impl Clone for HandleRef {
         Self {
             id: self.id,
             ref_type: match &self.ref_type {
-                Internal(sender) => {
+                Internal(sender) | Strong(sender) => {
                     let _ = sender.send(RefOp::Increase(self.id));
                     Strong(sender.clone())
                 },
-                Strong(sender) => Strong(sender.clone()),
                 Weak(sender) => Weak(sender.clone()),
                 None => panic!("unexpected ref type in clone()"),
             },
