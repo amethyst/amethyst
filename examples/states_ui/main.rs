2428cd7ae6db28117b84b80f07410b0d2ef1d2e1
Felix Karg <f.karg10@gmail.com>
2019-10-12 12:17:20 +0200

    added more comments

--- examples/states_ui/main.rs
+++ examples/states_ui/main.rs
@@ -35,32 +35,66 @@ mod welcome;
 pub fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
 
+    // this will be the directory the 'Cargo.toml' is defined in.
     let app_root = application_root_dir()?;
 
+    // our display config is in our configs folder.
     let display_config_path = app_root.join("examples/states_ui/config/display.ron");
+
+    // other assets ('*.ron' files, '*.png' textures, '*.ogg' audio files, ui prefab files, ...) are here
     let assets_dir = app_root.join("examples/assets");
 
     let game_data = GameDataBuilder::default()
+
+        // a lot of other bundles/systems depend on this (without it being explicitly clear), so it
+        // makes sense to add it early on
         .with_bundle(TransformBundle::new())?
+
+        // this bundle allows us to 'find' the Buttons and other UI elements later on
         .with_bundle(UiBundle::<StringBindings>::new())?
+
+        // this allows us to reload '*.ron' files during execution
         .with_bundle(HotReloadBundle::default())?
+
+        // without this System, our Program will silently (!) fail when trying to load the 'Game'.
+        // (try it!)
+        // This System takes care of Audio (in this case, the Button audio for hovering/clicking/...
         .with(Processor::<Source>::new(), "source_processor", &[])
+
+        // With this System, we can register UI events and act accordingly.
+        // In this example it simply prints the events, excluding it does not provide less
+        // functionality.
         .with_system_desc(
             crate::events::UiEventHandlerSystemDesc::default(),
             "ui_event_handler",
             &[],
         )
+
+        // This system is in 'events.rs'. Basically, it registers UI events that
+        // happen. Without it, the buttons will not react.
         .with_bundle(InputBundle::<StringBindings>::new())?
+
+        // Necessary for the FPS counter in the upper left corner to work.
+        // (simply uncommenting will fail at runtime, since the resource is expected to exist, you
+        // need to uncomment line 107-114 in game.rs for it to still work)
         .with_bundle(FpsCounterBundle)?
+
+        // Without this, we would not get a picture.
         .with_bundle(
             RenderingBundle::<DefaultBackend>::new()
+                // This creates the window and draws a background, if we don't specify a
+                // background in the loaded ui prefab file.
                 .with_plugin(
                     RenderToWindow::from_config_path(display_config_path)
                         .with_clear([0.005, 0.005, 0.005, 1.0]),
                 )
+                // Without this, all of our beautiful UI would not get drawn.
+                // It will work, but we won't see a thing.
                 .with_plugin(RenderUi::default()),
         )?;
 
+    // creating the Application with the assets_dir, the first Screen, and the game_data with it's
+    // systems.
     let mut game = Application::new(
         assets_dir,
         crate::welcome::WelcomeScreen::default(),
