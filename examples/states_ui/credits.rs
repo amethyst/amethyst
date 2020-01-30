8580bb18d778bbd15ea45da0a21efb58a0f2f7e5
Joël Lupien (Jojolepro) <jojolepromain@gmail.com>
2020-01-30 11:11:08 -0500

    rustfmt

--- examples/states_ui/credits.rs
+++ examples/states_ui/credits.rs
@@ -6,8 +6,7 @@ use amethyst::{
     winit::{MouseButton, VirtualKeyCode},
 };
 
-use crate::menu::MainMenu;
-use crate::util::delete_hierarchy;
+use crate::{menu::MainMenu, util::delete_hierarchy};
 
 // A simple 'Screen' State, only capable of loading/showing the prefab ui and registering simple
 // UI interactions (pressing escape or clicking anywhere).
