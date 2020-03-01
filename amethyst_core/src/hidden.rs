153cbfceae5d31a137e47b355e50bc1f2d054788
mvlabat <mvlabat@gmail.com>
2020-03-01 09:58:21 +0200

    Don't remove HiddenPropagate components set by users manually

--- amethyst_core/src/hidden.rs
+++ amethyst_core/src/hidden.rs
@@ -14,9 +14,32 @@ impl Component for Hidden {
 
 /// Like [Hidden](struct.Hidden.html), but can propagate through children when the [HideHierarchySystem](struct.HideHierarchySystem.html)
 /// is enabled in the [RenderBundle](struct.RenderBundle.html).
-#[derive(Clone, Debug, Default)]
-pub struct HiddenPropagate;
+#[derive(Clone, Debug)]
+pub struct HiddenPropagate {
+    pub(crate) was_propagated: bool,
+}
 
 impl Component for HiddenPropagate {
-    type Storage = FlaggedStorage<Self, NullStorage<Self>>;
+    type Storage = FlaggedStorage<Self>;
+}
+
+impl HiddenPropagate {
+    /// Creates an instance of HiddenPropagate.
+    pub fn new() -> Self {
+        Self {
+            was_propagated: false,
+        }
+    }
+
+    /// Is meant to be used only by HideHierarchySystem.
+    pub(crate) fn new_propagated() -> Self {
+        Self {
+            was_propagated: true,
+        }
+    }
+
+    /// Returns true if this component was propagated by [HideHierarchySystem](struct.HideHierarchySystem.html) automatically.
+    pub fn was_propagated(&self) -> bool {
+        self.was_propagated
+    }
 }
