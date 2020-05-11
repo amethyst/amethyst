dce5fd2d612db5fcabcc4ae01503c0c707974827
Azriel Hoh <azriel91@gmail.com>
2020-05-11 15:20:29 +1200

    Panics instead of returning `Err` in `HttpSource` to keep stack ok.
    See <https://github.com/rustwasm/wasm-bindgen/issues/1963>.

--- amethyst_assets/src/source/http.rs
+++ amethyst_assets/src/source/http.rs
@@ -5,9 +5,9 @@ use web_sys::{XmlHttpRequest, XmlHttpRequestResponseType};
 #[cfg(feature = "profiler")]
 use thread_profiler::profile_scope;
 
-use amethyst_error::{format_err, Error, ResultExt};
+use amethyst_error::Error;
 
-use crate::{error, source::Source};
+use crate::source::Source;
 
 /// HTTP source.
 ///
@@ -48,51 +48,46 @@ impl Source for HttpSource {
         #[cfg(feature = "profiler")]
         profile_scope!("http_load_asset");
 
+        // IMPORTANT!
+        //
+        // In WASM, functions that return `Result<_, JsValue>` can leak stack space.
+        // Therefore we should not return `Err(_)`s in this function in order for WASM applications to be "stable".
+        //
+        // See <https://github.com/rustwasm/wasm-bindgen/issues/1963>
+
         let path = self.path(path);
         let path_str = path
             .to_str()
-            .ok_or_else(|| format_err!("Path contains non-unicode characters: {:?}", path))
-            .with_context(|_| error::Error::Source)?;
+            .unwrap_or_else(|| panic!("Path contains non-unicode characters: {:?}", path));
 
         let xhr = XmlHttpRequest::new()
-            .map_err(|_| format_err!("Failed to construct XmlHttpRequest"))
-            .with_context(|_| error::Error::Source)?;
+            .unwrap_or_else(|e| panic!("Failed to construct XmlHttpRequest, {:?}", e));
 
         // Synchronous GET request. Should only be run in web worker.
         xhr.open_with_async("GET", path_str, false)
-            .map_err(|e| format_err!("XmlHttpRequest open failed: `{:?}`", e))
-            .with_context(|_| error::Error::Source)?;
+            .unwrap_or_else(|e| panic!("XmlHttpRequest open failed: `{:?}`", e));
         xhr.set_response_type(XmlHttpRequestResponseType::Arraybuffer);
 
         // We block here and wait for http fetch to complete
         xhr.send()
-            .map_err(|e| format_err!("XmlHttpRequest send failed: `{:?}`", e))
-            .with_context(|_| error::Error::Source)?;
+            .unwrap_or_else(|e| panic!("XmlHttpRequest send failed: `{:?}`", e));
 
         // Status returns a result but according to javascript spec it should never return error.
         // Returns 0 if request was not completed.
         let status = xhr
             .status()
-            .map_err(|e| format_err!("XmlHttpRequest `status` read failed: `{:?}`", e))
-            .with_context(|_| error::Error::Source)?;
+            .unwrap_or_else(|e| panic!("XmlHttpRequest `status` read failed: `{:?}`", e));
 
         if status != 200 {
             let msg = xhr
                 .status_text()
-                .map_err(|e| format_err!("XmlHttpRequest `status_text` read failed: `{:?}`", e))
-                .with_context(|_| error::Error::Source)?;
-            return Err(format_err!(
-                "XmlHttpRequest failed with code {}. Error: {}",
-                status,
-                msg
-            ))
-            .with_context(|_| error::Error::Source);
+                .unwrap_or_else(|e| panic!("XmlHttpRequest `status_text` read failed: `{:?}`", e));
+            panic!("XmlHttpRequest failed with code {}. Error: {}", status, msg);
         }
 
         let resp = xhr
             .response()
-            .map_err(|e| format_err!("XmlHttpRequest `response` read failed: `{:?}`", e))
-            .with_context(|_| error::Error::Source)?;
+            .unwrap_or_else(|e| panic!("XmlHttpRequest `response` read failed: `{:?}`", e));
 
         // Convert javascript ArrayBuffer into Vec<u8>
         let arr = Uint8Array::new(&resp);
