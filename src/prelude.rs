f96f2d9f899b59883037e771bbda6041ab343740
David Kushner <root@davidkushner.me>
2020-06-21 20:29:36 -0700

    Remove latent references to CallbackQueue.

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,7 +2,6 @@
 
 pub use crate::{
     app::{Application, ApplicationBuilder, CoreApplication},
-    callback_queue::{Callback, CallbackQueue},
     config::Config,
     ecs::prelude::*,
     game_data::{DataInit, GameData, GameDataBuilder},
