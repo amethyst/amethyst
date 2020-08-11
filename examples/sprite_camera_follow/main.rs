7d92fb56675e048ef5455633f8bdcd7ca4bcb327
Colin Woodbury <colin@fosskers.ca>
2020-08-11 16:27:14 -0700

    [examples] Use `SpriteRender::new()`

--- examples/sprite_camera_follow/main.rs
+++ examples/sprite_camera_follow/main.rs
@@ -68,10 +68,7 @@ fn load_sprite_sheet(world: &mut World, png_path: &str, ron_path: &str) -> Handl
 fn init_background_sprite(world: &mut World, sprite_sheet: &Handle<SpriteSheet>) -> Entity {
     let mut transform = Transform::default();
     transform.set_translation_z(-10.0);
-    let sprite = SpriteRender {
-        sprite_sheet: sprite_sheet.clone(),
-        sprite_number: 0,
-    };
+    let sprite = SpriteRender::new(sprite_sheet.clone(), 0);
     world
         .create_entity()
         .with(transform)
@@ -85,10 +82,7 @@ fn init_background_sprite(world: &mut World, sprite_sheet: &Handle<SpriteSheet>)
 fn init_reference_sprite(world: &mut World, sprite_sheet: &Handle<SpriteSheet>) -> Entity {
     let mut transform = Transform::default();
     transform.set_translation_xyz(0.0, 0.0, 0.0);
-    let sprite = SpriteRender {
-        sprite_sheet: sprite_sheet.clone(),
-        sprite_number: 0,
-    };
+    let sprite = SpriteRender::new(sprite_sheet.clone(), 0);
     world
         .create_entity()
         .with(transform)
@@ -102,10 +96,7 @@ fn init_reference_sprite(world: &mut World, sprite_sheet: &Handle<SpriteSheet>) 
 fn init_screen_reference_sprite(world: &mut World, sprite_sheet: &Handle<SpriteSheet>) -> Entity {
     let mut transform = Transform::default();
     transform.set_translation_xyz(-250.0, -245.0, -11.0);
-    let sprite = SpriteRender {
-        sprite_sheet: sprite_sheet.clone(),
-        sprite_number: 0,
-    };
+    let sprite = SpriteRender::new(sprite_sheet.clone(), 0);
     world
         .create_entity()
         .with(transform)
@@ -118,10 +109,7 @@ fn init_screen_reference_sprite(world: &mut World, sprite_sheet: &Handle<SpriteS
 fn init_player(world: &mut World, sprite_sheet: &Handle<SpriteSheet>) -> Entity {
     let mut transform = Transform::default();
     transform.set_translation_xyz(0.0, 0.0, -3.0);
-    let sprite = SpriteRender {
-        sprite_sheet: sprite_sheet.clone(),
-        sprite_number: 1,
-    };
+    let sprite = SpriteRender::new(sprite_sheet.clone(), 1);
     world
         .create_entity()
         .with(transform)
