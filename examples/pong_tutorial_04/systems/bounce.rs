23ecd9a16cc87d60638ae446d676980eccef74d0
François Mockers <mockersf@gmail.com>
2020-09-12 14:31:06 +0200

    maybe better solution for pong step 4 bounce system
    keep coordinates of the paddle in the Paddle component as well as in the Transform

--- examples/pong_tutorial_04/systems/bounce.rs
+++ examples/pong_tutorial_04/systems/bounce.rs
@@ -9,35 +9,19 @@ use crate::pong::{Ball, Paddle, Side, ARENA_HEIGHT};
 pub fn build() -> impl Runnable {
     SystemBuilder::new("BounceSystem")
         .with_query(<(&mut Ball, &Transform)>::query())
-        // .with_query(<(&Entity, &mut Ball)>::query())
-        .with_query(<(&Paddle, &Transform)>::query())
+        .with_query(<&Paddle>::query())
         .read_component::<Paddle>()
         .read_component::<Transform>()
         .write_component::<Ball>()
         .build(
             move |_commands, world, _resources, (query_balls, query_paddles)| {
-                // Get the coordinates for each paddle. It is done outside of `query_balls` loop, as it borrows `world`
-                // as immutable while `query_balls` borrows it as mutable
-                let paddles = query_paddles
-                    .iter(world)
-                    .map(|(paddle, paddle_transform)| {
-                        let paddle_x = paddle_transform.translation().x - (paddle.width * 0.5);
-                        let paddle_y = paddle_transform.translation().y - (paddle.height * 0.5);
-                        (
-                            paddle_x,
-                            paddle_y,
-                            paddle.width,
-                            paddle.height,
-                            paddle.side.clone(),
-                        )
-                    })
-                    .collect::<Vec<_>>();
+                let (mut ball_world, remaining) = world.split_for_query(query_balls);
 
                 // Check whether a ball collided, and bounce off accordingly.
                 //
                 // We also check for the velocity of the ball every time, to prevent multiple collisions
                 // from occurring.
-                for (ball, transform) in query_balls.iter_mut(world) {
+                for (ball, transform) in query_balls.iter_mut(&mut ball_world) {
                     let ball_x = transform.translation().x;
                     let ball_y = transform.translation().y;
 
@@ -48,10 +32,8 @@ pub fn build() -> impl Runnable {
                         ball.velocity[1] = -ball.velocity[1];
                     }
 
-                    // // Bounce at the paddles.
-                    for (paddle_x, paddle_y, paddle_width, paddle_height, paddle_side) in
-                        paddles.iter()
-                    {
+                    // Bounce at the paddles.
+                    for paddle in query_paddles.iter(&remaining) {
                         // To determine whether the ball has collided with a paddle, we create a larger
                         // rectangle around the current one, by subtracting the ball radius from the
                         // lowest coordinates, and adding the ball radius to the highest ones. The ball
@@ -60,12 +42,12 @@ pub fn build() -> impl Runnable {
                         if point_in_rect(
                             ball_x,
                             ball_y,
-                            paddle_x - ball.radius,
-                            paddle_y - ball.radius,
-                            paddle_x + (paddle_width + ball.radius),
-                            paddle_y + (paddle_height + ball.radius),
-                        ) && ((*paddle_side == Side::Left && ball.velocity[0] < 0.0)
-                            || (*paddle_side == Side::Right && ball.velocity[0] > 0.0))
+                            paddle.x - paddle.width / 2. - ball.radius,
+                            paddle.y - paddle.height / 2. - ball.radius,
+                            paddle.x + paddle.width / 2. + ball.radius,
+                            paddle.y + paddle.height / 2. + ball.radius,
+                        ) && ((paddle.side == Side::Left && ball.velocity[0] < 0.0)
+                            || (paddle.side == Side::Right && ball.velocity[0] > 0.0))
                         {
                             ball.velocity[0] = -ball.velocity[0];
                         }
