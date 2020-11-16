d25ce3bb5f67dc24179a31b3a331b67a07df10fd
Tudor Lechintan <lechintantudor02@gmail.com>
2020-11-16 20:50:33 +0200

    Make the code compile

--- amethyst_core/src/lib.rs
+++ amethyst_core/src/lib.rs
@@ -6,56 +6,48 @@
 #[cfg(all(target_os = "emscripten", not(no_threading)))]
 compile_error!("the cfg flag \"no_threading\" is required when building for emscripten");
 
-#[macro_use]
-extern crate getset;
+/// A rayon thread pool wrapped in an `Arc`. This should be used as resource.
+pub type ArcThreadPool = std::sync::Arc<rayon::ThreadPool>;
 
-pub use alga;
-pub use approx;
+pub use self::{
+    axis::{Axis2, Axis3},
+    event::EventReader,
+    hidden::{Hidden, HiddenPropagate},
+    named::Named,
+    shrev::EventChannel,
+    timing::*,
+};
 pub use nalgebra as math;
 pub use num_traits as num;
 pub use shrev;
 
-use std::sync::Arc;
-
-pub use crate::{event::EventReader, timing::*};
-
-pub use self::{
-    axis::{Axis2, Axis3},
-    hidden::{Hidden, HiddenPropagate},
-    named::Named,
-    shrev::EventChannel,
-};
+/// legion ECS reexported with some convenience types.
+pub mod ecs {
+    pub use crate::dispatcher::{Dispatcher, DispatcherBuilder, SystemBundle};
+    pub use legion::{
+        systems::{CommandBuffer, Resource, Runnable},
+        world::SubWorld,
+        *,
+    };
+}
 
 /// Dispatcher module.
 pub mod dispatcher;
+
 /// The frame limiter module.
 pub mod frame_limiter;
+
 /// The geometry module.
 pub mod geometry;
+
 /// The timing module.
 pub mod timing;
+
 /// The transformation module.
 pub mod transform;
-
-use legion;
-
-/// legion ECS reexported with some convenience types.
-pub mod ecs {
-    pub use crate::{
-        dispatcher::{Dispatcher, DispatcherBuilder, SystemBundle},
-        legion::{
-            systems::{CommandBuffer, Resource, Runnable},
-            world::SubWorld,
-            *,
-        },
-    };
-}
 
 mod axis;
 mod event;
 mod hidden;
 //mod hide_system;
 mod named;
-
-/// A rayon thread pool wrapped in an `Arc`. This should be used as resource in `World`.
-pub type ArcThreadPool = Arc<rayon::ThreadPool>;
