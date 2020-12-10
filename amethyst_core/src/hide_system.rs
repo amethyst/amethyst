7f796b5e72dd38ae5499bddb744a8dbcf0d88b4d
Emory Petermann <emory@onlyfor.us>
2020-12-09 22:42:15 -0600

    port HideHierarchySystem to legion

--- amethyst_core/src/hide_system.rs
+++ amethyst_core/src/hide_system.rs
@@ -1,155 +1,62 @@
-use crate::{
-    ecs::prelude::{
-        BitSet, ComponentEvent, ReadExpect, ReadStorage, ReaderId, System, SystemData, World,
-        WriteStorage,
-    },
-    transform::components::{HierarchyEvent, Parent, ParentHierarchy},
-    SystemDesc,
-};
-
 #[cfg(feature = "profiler")]
 use thread_profiler::profile_scope;
 
-use log::error;
-
-use crate::HiddenPropagate;
-
-/// Builds a `HideHierarchySystem`.
-#[derive(Default, Debug)]
-pub struct HideHierarchySystemDesc;
-
-impl<'a, 'b> SystemDesc<'a, 'b, HideHierarchySystem> for HideHierarchySystemDesc {
-    fn build(self, world: &mut World) -> HideHierarchySystem {
-        <HideHierarchySystem as System<'_>>::SystemData::setup(world);
-
-        // This fetch_mut panics if `ParentHierarchy` is not set up yet, hence the dependency on
-        // "parent_hierarchy_system"
-        let parent_events_id = world.fetch_mut::<ParentHierarchy>().track();
-        let mut hidden = WriteStorage::<HiddenPropagate>::fetch(&world);
-        let hidden_events_id = hidden.register_reader();
-
-        HideHierarchySystem::new(hidden_events_id, parent_events_id)
-    }
-}
+use crate::{ecs::*, transform::components::Parent, HiddenPropagate};
 
 /// This system adds a [HiddenPropagate](struct.HiddenPropagate.html)-component to all children.
 ///
 /// Using this system will result in every child being hidden.
-/// Depends on the resource "ParentHierarchy", which is set up by the
-/// [TransformBundle](struct.TransformBundle.html)
-///
-/// Based on the [UiTransformSystem](struct.UiTransformSystem.html).
 #[derive(Debug)]
-pub struct HideHierarchySystem {
-    marked_as_modified: BitSet,
-    manually_hidden: BitSet,
-    hidden_events_id: ReaderId<ComponentEvent>,
-    parent_events_id: ReaderId<HierarchyEvent>,
-}
+pub struct HideHierarchySystem;
 
 impl HideHierarchySystem {
     /// Creates a new `HideHierarchySystem`.
-    pub fn new(
-        hidden_events_id: ReaderId<ComponentEvent>,
-        parent_events_id: ReaderId<HierarchyEvent>,
-    ) -> Self {
-        Self {
-            marked_as_modified: BitSet::default(),
-            manually_hidden: BitSet::default(),
-            hidden_events_id,
-            parent_events_id,
-        }
+    pub fn new() -> Self {
+        Self {}
     }
-}
 
-impl<'a> System<'a> for HideHierarchySystem {
-    type SystemData = (
-        WriteStorage<'a, HiddenPropagate>,
-        ReadStorage<'a, Parent>,
-        ReadExpect<'a, ParentHierarchy>,
-    );
-    fn run(&mut self, (mut hidden, parents, hierarchy): Self::SystemData) {
-        #[cfg(feature = "profiler")]
-        profile_scope!("hide_hierarchy_system");
+    pub fn build(&'static mut self) -> Box<dyn Runnable> {
+        Box::new(
+            SystemBuilder::<()>::new("UiTransformSystem")
+                .with_query(
+                    <(Entity, TryWrite<HiddenPropagate>, Read<Parent>)>::query()
+                        .filter(maybe_changed::<HiddenPropagate>()),
+                )
+                .build(move |commands, world, _, query| {
+                    #[cfg(feature = "profiler")]
+                    profile_scope!("hide_hierarchy_system");
 
-        self.marked_as_modified.clear();
-        self.manually_hidden.clear();
+                    // collect the hidden things
+                    let propagates: Vec<(Entity, HiddenPropagate)> = {
+                        <(Entity, Read<HiddenPropagate>)>::query()
+                            .iter(world)
+                            .map(|x| (*x.0, x.1.clone()))
+                            .collect()
+                    };
 
-        // Borrow multiple parts of self as mutable
-        let self_hidden_events_id = &mut self.hidden_events_id;
-        let self_marked_as_modified = &mut self.marked_as_modified;
-        let self_manually_hidden = &mut self.manually_hidden;
+                    // attaches HiddenPropagate to children
+                    for (entity, hidden, parent) in query.iter_mut(world) {
+                        {
+                            let self_is_manually_hidden =
+                                hidden.map_or(false, |p| !p.is_propagated);
 
-        hidden
-            .channel()
-            .read(self_hidden_events_id)
-            .for_each(|event| match event {
-                ComponentEvent::Inserted(id) | ComponentEvent::Removed(id) => {
-                    self_marked_as_modified.add(*id);
-                }
-                ComponentEvent::Modified(_id) => {}
-            });
+                            let hidden_parent = propagates.iter().find(|x| x.0 == parent.0);
 
-        for event in hierarchy.changed().read(&mut self.parent_events_id) {
-            match *event {
-                HierarchyEvent::Removed(entity) => {
-                    self_marked_as_modified.add(entity.id());
-                }
-                HierarchyEvent::Modified(entity) => {
-                    // HierarchyEvent::Modified includes insertion of new components to the storage.
-                    self_marked_as_modified.add(entity.id());
-                }
-            }
-        }
-
-        // Compute hide status with parents.
-        for entity in hierarchy.all() {
-            {
-                let self_hidden = hidden.get(*entity);
-                let self_is_manually_hidden =
-                    self_hidden.as_ref().map_or(false, |p| !p.is_propagated);
-                if self_is_manually_hidden {
-                    self_manually_hidden.add(entity.id());
-                }
-
-                let parent_entity = parents
-                    .get(*entity)
-                    .expect(
-                        "Unreachable: All entities in `ParentHierarchy` should also be in \
-                         `Parents`",
-                    )
-                    .entity;
-                let parent_dirty = self_marked_as_modified.contains(parent_entity.id());
-                let parent_hidden = hidden.get(parent_entity);
-                let parent_is_manually_hidden =
-                    parent_hidden.as_ref().map_or(false, |p| !p.is_propagated);
-                if parent_is_manually_hidden {
-                    self_manually_hidden.add(parent_entity.id());
-                    self_manually_hidden.add(entity.id());
-                }
-
-                if parent_dirty && !self_is_manually_hidden {
-                    if parent_hidden.is_some() {
-                        if let Err(e) = hidden.insert(*entity, HiddenPropagate::new_propagated()) {
-                            error!("Failed to automatically add `HiddenPropagate`: {:?}", e);
+                            if !self_is_manually_hidden {
+                                if hidden_parent.is_some() {
+                                    commands.add_component(
+                                        *entity,
+                                        HiddenPropagate {
+                                            is_propagated: true,
+                                        },
+                                    );
+                                } else {
+                                    commands.remove_component::<HiddenPropagate>(*entity);
+                                }
+                            }
                         }
-                    } else {
-                        hidden.remove(*entity);
                     }
-                }
-            }
-            // Populate the modifications we just did.
-            // Happens inside the for-loop, so that the changes are picked up in the next iteration
-            // already, instead of on the next `system.run()`
-            hidden
-                .channel()
-                .read(self_hidden_events_id)
-                .for_each(|event| match event {
-                    ComponentEvent::Inserted(id) | ComponentEvent::Removed(id) => {
-                        self_marked_as_modified.add(*id);
-                    }
-                    ComponentEvent::Modified(_id) => {}
-                });
-        }
+                }),
+        )
     }
 }
