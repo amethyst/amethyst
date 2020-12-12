4455c73f498555850916a362519e60aee806755a
grzi <jeremy.thulliez@gmail.com>
2020-12-12 14:00:39 +0100

    feat: convert resize system to legion

--- amethyst_ui/src/resize.rs
+++ amethyst_ui/src/resize.rs
@@ -1,16 +1,12 @@
 use amethyst_core::{
-    ecs::prelude::{
-        BitSet, Component, ComponentEvent, FlaggedStorage, Join, ReadExpect, System, SystemData,
-        WriteStorage,
-    },
-    shrev::ReaderId,
+    ecs::*,
 };
-use amethyst_derive::SystemDesc;
 use amethyst_window::ScreenDimensions;
 #[cfg(feature = "profiler")]
 use thread_profiler::profile_scope;
 
 use super::*;
+use amethyst_core::ecs::systems::ParallelRunnable;
 
 /// Whenever the window is resized the function in this component will be called on this
 /// entity's UiTransform, along with the new width and height of the window.
@@ -26,8 +22,8 @@ pub struct UiResize {
 impl UiResize {
     /// Creates a new component with the given function.
     pub fn new<F>(function: F) -> Self
-    where
-        F: FnMut(&mut UiTransform, (f32, f32)) + Send + Sync + 'static,
+        where
+            F: FnMut(&mut UiTransform, (f32, f32)) + Send + Sync + 'static,
     {
         UiResize {
             function: Box::new(function),
@@ -35,87 +31,53 @@ impl UiResize {
     }
 }
 
-impl Component for UiResize {
-    type Storage = FlaggedStorage<Self>;
-}
-
 /// This system rearranges UI elements whenever the screen is resized using their `UiResize`
 /// component.
-#[derive(Debug, SystemDesc)]
-#[system_desc(name(ResizeSystemDesc))]
+#[derive(Debug)]
 pub struct ResizeSystem {
-    #[system_desc(skip)]
-    screen_size: (f32, f32),
-    #[system_desc(flagged_storage_reader(UiResize))]
-    resize_events_id: ReaderId<ComponentEvent>,
-    #[system_desc(skip)]
-    local_modified: BitSet,
+    screen_size: (f32, f32)
 }
 
 impl ResizeSystem {
     /// Creates a new ResizeSystem that listens with the given reader Id.
-    pub fn new(resize_events_id: ReaderId<ComponentEvent>) -> ResizeSystem {
+    pub fn new() -> ResizeSystem {
         let screen_size = (0.0, 0.0);
 
         ResizeSystem {
             screen_size,
-            resize_events_id,
-            local_modified: BitSet::default(),
         }
     }
-}
 
-impl<'a> System<'a> for ResizeSystem {
-    type SystemData = (
-        WriteStorage<'a, UiTransform>,
-        WriteStorage<'a, UiResize>,
-        ReadExpect<'a, ScreenDimensions>,
-    );
+    pub fn build(&mut self) -> Box<dyn ParallelRunnable> {
+        Box::new(
+            SystemBuilder::new("ResizeSystem")
+                .with_query(<(&mut UiTransform, &mut UiResize)>::query()
+                        .filter(maybe_changed::<UiResize>()),
+                )
+                .with_query(<(&mut UiTransform, &mut UiResize)>::query())
+                .read_resource::<ScreenDimensions>()
+                .build(move |(_commands, world, screen_dimensions, (resized, all_with_resize))| {
+                    #[cfg(feature = "profiler")]
+                    profile_scope!("resize_system");
+                    self.local_modified.clear();
+                    let self_local_modified = &mut self.local_modified;
+                    let self_resize_events_id = &mut self.resize_events_id;
+                    let screen_size = (screen_dimensions.width() as f32, screen_dimensions.height() as f32);
 
-    fn run(&mut self, (mut transform, mut resize, dimensions): Self::SystemData) {
-        #[cfg(feature = "profiler")]
-        profile_scope!("resize_system");
-
-        self.local_modified.clear();
-
-        let self_local_modified = &mut self.local_modified;
-
-        let self_resize_events_id = &mut self.resize_events_id;
-        resize
-            .channel()
-            .read(self_resize_events_id)
-            .for_each(|event| match event {
-                ComponentEvent::Inserted(id) | ComponentEvent::Modified(id) => {
-                    self_local_modified.add(*id);
-                }
-                ComponentEvent::Removed(_id) => {}
-            });
-
-        let screen_size = (dimensions.width() as f32, dimensions.height() as f32);
-        if self.screen_size != screen_size {
-            self.screen_size = screen_size;
-            for (transform, resize) in (&mut transform, &mut resize).join() {
-                (resize.function)(transform, screen_size);
-            }
-        } else {
-            // Immutable borrow
-            let self_local_modified = &*self_local_modified;
-            for (transform, resize, _) in (&mut transform, &mut resize, self_local_modified).join()
-            {
-                (resize.function)(transform, screen_size);
-            }
-        }
-
-        // We need to treat any changes done inside the system as non-modifications, so we read out
-        // any events that were generated during the system run
-        resize
-            .channel()
-            .read(self_resize_events_id)
-            .for_each(|event| match event {
-                ComponentEvent::Inserted(id) | ComponentEvent::Modified(id) => {
-                    self_local_modified.add(*id);
-                }
-                ComponentEvent::Removed(_id) => {}
-            });
+                    // If the screen_size changed, we resize everything with UiResize
+                    if self.screen_size != screen_size {
+                        self.screen_size = screen_size;
+                        all_with_resize.for_each_mut(world, |mut transform, mut resize| {
+                            (resize.function)(transform, screen_size);
+                        });
+                    }
+                    // Else, we only try to resize the modified ones
+                    else{
+                        resized.for_each_mut(world, |mut transform, mut resize| {
+                            (resize.function)(transform, screen_size);
+                        });
+                    }
+                })
+        )
     }
 }
