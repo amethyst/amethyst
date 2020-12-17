6eac5d7b4e2be2fa43b785b8c8dafbbd051f1211
Benlitz <dev@benlitz.net>
2020-12-17 17:47:33 +0900

    Fix Plane::intersect_line function that was returning a distance with the wrong sign

--- amethyst_core/src/geometry.rs
+++ amethyst_core/src/geometry.rs
@@ -95,8 +95,8 @@ where
     /// Returns the intersection distance of the provided line given a point and direction, or `None` if none occurs.
     pub fn intersect_line(&self, point: &Point3<T>, direction: &Vector3<T>) -> Option<T> {
         let fv = self.dot(direction);
-        let distance = self.dot_point(point) / fv;
-        if fv.abs() > T::min_value() {
+        if fv.abs().is_positive() {
+            let distance = -self.dot_point(point) / fv;
             Some(distance)
         } else {
             None
@@ -131,7 +131,7 @@ where
 
     /// Returns a `Point` along the ray at a distance `t` from it's origin.
     pub fn at_distance(&self, z: T) -> Point3<T> {
-        self.origin - (self.direction * z)
+        self.origin + (self.direction * z)
     }
 }
 
