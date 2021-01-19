4584d37ac442352c2830cdfc9025a0b743c18b3e
James Lowry <jlowry@users.noreply.github.com>
2021-01-19 15:06:56 +0100

    Remove previous version prefab entities
    Added entity_map to PrefabInstance so that previously spawned entities are
    correctly removed when no longer needed.

--- amethyst_assets/src/prefab/system.rs
+++ amethyst_assets/src/prefab/system.rs
@@ -11,6 +11,7 @@ use crate::{
 
 struct PrefabInstance {
     version: u32,
+    entity_map: HashMap<Entity, Entity, EntityHasher>,
 }
 
 /// Attaches prefabs to entities that have Handle<Prefab>
@@ -35,35 +36,24 @@ pub fn prefab_spawning_tick(world: &mut World, resources: &mut Resources) {
     <(Entity, &Handle<Prefab>, TryWrite<PrefabInstance>)>::query().for_each_mut(
         world,
         |(entity, handle, instance)| {
-            if let Some(prefab) = prefab_storage.get(handle) {
-                if let Some(cooked_prefab) = prefab.cooked.as_ref() {
-                    if let Some(instance) = instance {
-                        if instance.version < prefab.version {
-                            log::debug!("Updating existing prefab.");
-                            let mut map = HashMap::<Entity, Entity, EntityHasher>::default();
-                            if let Some((root_entity,)) =
-                                entity_query.iter(&cooked_prefab.world).next()
-                            {
-                                map.insert(*root_entity, *entity);
-                            };
-
-                            prefabs.push((
-                                *entity,
-                                cooked_prefab,
-                                prefab.version,
-                                map,
-                                handle.clone(),
-                            ));
-                        }
-                    } else {
-                        log::debug!("Spawning new prefab.");
-                        let mut map = HashMap::<Entity, Entity, EntityHasher>::default();
+            if let Some(Prefab {
+                prefab: Some(cooked_prefab),
+                version: prefab_version,
+                ..
+            }) = prefab_storage.get(handle)
+            {
+                let instance_version = instance.map(|instance| instance.version).unwrap_or(0);
+                if instance_version < *prefab_version {
+                    let mut entity_map = instance
+                        .map(|instance| instance.entity_map.clone())
+                        .unwrap_or_default();
+                    if entity_map.is_empty() {
                         if let Some((root_entity,)) = entity_query.iter(&cooked_prefab.world).next()
                         {
-                            map.insert(*root_entity, *entity);
-                        };
-                        prefabs.push((*entity, cooked_prefab, prefab.version, map, handle.clone()));
+                            entity_map.insert(*root_entity, *entity);
+                        }
                     }
+                    prefabs.push((*entity, cooked_prefab, *prefab_version, entity_map));
                 }
             }
         },
@@ -82,8 +72,8 @@ pub fn prefab_spawning_tick(world: &mut World, resources: &mut Resources) {
         log::debug!("new entity_map: {:?}", entity_map);
         log::debug!("old entity map: {:?}", prev_entity_map);
 
-        for value in prev_entities.difference(&live_entities) {
-            if world.remove(*value) {
+        for value in prev_entities.difference(&live_entities).copied() {
+            if world.remove(value) {
                 log::debug!("Removed entity {:?}", value)
             }
         }
@@ -91,8 +81,10 @@ pub fn prefab_spawning_tick(world: &mut World, resources: &mut Resources) {
         log::debug!("Spawn for {:?}", entity);
 
         if let Some(mut entry) = world.entry(entity) {
-            entry.add_component(PrefabInstance { version });
-            entry.add_component(handle);
+            entry.add_component(PrefabInstance {
+                version,
+                entity_map,
+            });
         } else {
             log::error!("Could not update entity");
         }
